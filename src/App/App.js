bbc4f8f2fd6177c376ae4ad17ca9fa4e8183b654
Julian <julian.weise@student.hpi.uni-potsdam.de>
2017-01-12 10:57:13 +0100

    Integrated searchfunctionality.

--- src/App/App.js
+++ src/App/App.js
@@ -22,7 +22,7 @@ class App extends Component {
     }
 
     render() {
-        let listOfPorducts = JSON.parse('[{"numberOfDevices":254,"numberOfEvents":2001,"productionStart":"Feb 1, 2016 12:00:00 AM","state":"RUNNING","name":"example family","id":47, "metaData": {"label":"product label 001", "brand":"Testbrand1", "orderNumber":1234, "statusDescription":"everything is broken!"}}, {"numberOfDevices":1337,"numberOfEvents":9001,"productionStart":"Feb 1, 4099 12:00:00 AM","state":"ERROR","name":"example family","id":42, "metaData": {"label":"product label 001", "brand":"Testbrand1", "orderNumber":1234, "statusDescription":"everything is broken!"}}]');
+        let listOfPorducts = JSON.parse('[{"numberOfDevices":254,"numberOfEvents":2001,"productionStart":"Feb 1, 2016 12:00:00 AM","state":"RUNNING","name":"example family","id":47, "metaData": {"label":"product label 002", "brand":"Testbrand1", "orderNumber":1234, "statusDescription":"everything is broken!"}}, {"numberOfDevices":1337,"numberOfEvents":9001,"productionStart":"Feb 1, 4099 12:00:00 AM","state":"ERROR","name":"example family","id":42, "metaData": {"label":"product label 001", "brand":"Testbrand1", "orderNumber":1234, "statusDescription":"everything is broken!"}}]');
         console.log(listOfPorducts);
         return (
             <div>
