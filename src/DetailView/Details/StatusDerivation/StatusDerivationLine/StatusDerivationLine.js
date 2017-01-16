c8631f746a30122308e9963d1127accc7fb51a06
Christian Friedow <christian@friedow.com>
2017-01-16 15:58:50 +0100

    Skeleton code for Details

---
+++ src/DetailView/Details/StatusDerivation/StatusDerivationLine/StatusDerivationLine.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './StatusDerivationLine.css';
+
+class StatusDerivationLine extends Component {
+    render() {
+        return (<div></div>
+        );
+    }
+}
+
+export default StatusDerivationLine;
