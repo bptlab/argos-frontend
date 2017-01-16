8f1c409bfdf4f7d951fdd2a922f268e84745c11b
Christian Friedow <christian@friedow.com>
2017-01-16 15:59:49 +0100

    Skeleton code for Header

---
+++ src/DetailView/Header/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+    render() {
+        return (<div></div>
+        );
+    }
+}
+
+export default Header;
