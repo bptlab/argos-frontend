f10889f65008267e728c989208dba2d3e938e957
Christian Friedow <christian@friedow.com>
2017-01-16 15:59:25 +0100

    Skeleton code for Filterbar

---
+++ src/DetailView/Filterbar/Filter/Filter.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './Filter.css';
+
+class Filter extends Component {
+    render() {
+        return (<div></div>
+        );
+    }
+}
+
+export default Filter;
