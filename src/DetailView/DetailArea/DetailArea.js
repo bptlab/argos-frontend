de6424010ac1d5f98fa84fbaa3f21cdc1536acb3
Christian Friedow <christian@friedow.com>
2017-01-16 16:46:33 +0100

    Renamed Details to DetailArea

---
+++ src/DetailView/DetailArea/DetailArea.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './DetailArea.css';
+
+class DetailArea extends Component {
+    render() {
+        return (<div></div>
+        );
+    }
+}
+
+export default DetailArea;
