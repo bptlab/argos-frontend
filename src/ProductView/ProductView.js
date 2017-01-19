1dce0a0c0483a0e2661b0688f5182a10b6a31bb3
Christian Friedow <christian@friedow.com>
2017-01-19 11:15:27 +0100

    Implemented HTML for ProductView.js

--- src/ProductView/ProductView.js
+++ src/ProductView/ProductView.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import './ProductView.css';
+import Header from 'Header/Header.js';
+import DetailArea from 'DetailArea/DetailArea.js';
+import Filterbar from 'Filterbar/Filterbar.js';
+import Tabbar from 'Tabbar/Tabbar.js';
+import EventList from 'EventList/EventList.js';
 
 class ProductView extends Component {
     render() {
-        return (<div></div>
+        return (
+            <div>
+                <Header/>
+                <DetailArea/>
+                <Filterbar/>
+                <Tabbar/>
+                <EventList/>
+            </div>
         );
     }
 }
