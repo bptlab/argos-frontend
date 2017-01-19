3dbaca50f47b217e57b73379950c6e9909213355
Christian Friedow <christian@friedow.com>
2017-01-19 10:48:29 +0100

    Added routing for dashboard and detail view

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
 import App from './App/App';
 import DetailView from './DetailView/DetailView';
+import DashboardView from './DashboardView/DashboardView';
 import './index.css';
 
 ReactDOM.render(
     (<Router history={hashHistory}>
-        <Route path="/" component={App} />
-        <Route path="/product/:productID" component={DetailView} />
+        <Route path="/" component={App}>
+            <IndexRoute component={DashboardView}/>
+            <Route path="/product/:productID" component={DetailView}/>
+        </Route>
     </Router>),
   document.getElementById('root')
 );
