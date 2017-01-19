acac8b16c67c5d7b921ece3543dbdea5300a834e
Christian Friedow <christian@friedow.com>
2017-01-19 11:54:33 +0100

    Implemented HTML Header

--- src/ProductView/Header/Header.js
+++ src/ProductView/Header/Header.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import './Header.css';
+import StatusPoint from './StatusPoint/StatusPoint.js';
 
 class Header extends Component {
     render() {
-        return (<div></div>
+        return (
+            <div>
+                <a className="btn btn-secondary" href="/" role="button"><i class="fa fa-home" aria-hidden="true"></i>TEST</a>
+                <h1>{this.props.product.metaData.label}</h1>
+                <StatusPoint product={this.props.product}/>
+            </div>
         );
     }
 }
--- src/ProductView/ProductView.js
+++ src/ProductView/ProductView.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import './ProductView.css';
-import Header from 'Header/Header.js';
-import DetailArea from 'DetailArea/DetailArea.js';
-import Filterbar from 'Filterbar/Filterbar.js';
-import Tabbar from 'Tabbar/Tabbar.js';
-import EventList from 'EventList/EventList.js';
+import Header from './Header/Header.js';
+import DetailArea from './DetailArea/DetailArea.js';
+import Filterbar from './Filterbar/Filterbar.js';
+import Tabbar from './Tabbar/Tabbar.js';
+import EventList from './EventList/EventList.js';
 
 class ProductView extends Component {
     render() {
+        const product = JSON.parse('{"numberOfDevices":83,"numberOfEvents":2001,"productionStart":"Feb 1, 2016 12:00:00 AM","state":"RUNNING","name":"example family","id":483, "metaData": {"label":"product label 002", "brand":"Testbrand2", "orderNumber":1234, "statusDescription":"everything is broken!"}}');
         return (
             <div>
-                <Header/>
+                <Header product={product}/>
                 <DetailArea/>
                 <Filterbar/>
                 <Tabbar/>
