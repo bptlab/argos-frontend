2aff237502e241bb1c9e76729518931b4fbd5cf0
Christian Friedow <christian@friedow.com>
2017-01-19 09:57:22 +0100

    Minimal working routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router'
 import App from './App/App';
+import DetailView from './DetailView/DetailView';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+    (<Router history={hashHistory}>
+        <Route path="/" component={App} />
+        <Route path="/product" component={DetailView} />
+    </Router>),
   document.getElementById('root')
 );
