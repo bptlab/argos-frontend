f821377da269bad3cebba8341b59534fd1bd9bac
Christian Friedow <christian@friedow.com>
2017-01-23 11:05:16 +0100

    Included jquery, tether and bootstrap

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,19 @@
 import React, {Component} from 'react';
 import './App.css';
-import './bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// Importing jQuery in ES6 style
+import $ from "jquery";
+
+// We need to expose jQuery as global variable
+window.jQuery = window.$ = $;
+
+import tether from 'tether';
+global.Tether = tether;
+
+// ES6 import does not work it throws error: Missing jQuery
+// using Node.js style import works without problems
+require('bootstrap');
 
 class App extends Component {
     render() {
