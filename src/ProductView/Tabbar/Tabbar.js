aec88a37404ad51805abdd3a266a88ae5e633bdb
Christian Friedow <christian@friedow.com>
2017-01-23 13:05:56 +0100

    Added HTML for Tabbar

--- src/ProductView/Tabbar/Tabbar.js
+++ src/ProductView/Tabbar/Tabbar.js
@@ -3,7 +3,12 @@ import './Tabbar.css';
 
 class Tabbar extends Component {
     render() {
-        return (<div></div>
+        return (
+            <div>
+                <ul className="nav nav-tabs" role="tablist">
+                    {this.props.eventTypes.map((eventType, index) => <li className="nav-item"><a className="nav-link active" data-toggle="tab" href={'#' + eventType.name} role="tab">{eventType.name}</a></li>)}
+                </ul>
+            </div>
         );
     }
 }
