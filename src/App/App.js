52886522718722a32fdf3b654eb5dc5d736d9985
Christian Friedow <christian@friedow.com>
2017-01-25 11:16:13 +0100

    Added font-awesome

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,11 @@
 import React, {Component} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-
-// Importing jQuery in ES6 style
+import 'font-awesome/css/font-awesome.css'
 import $ from "jquery";
-
-// We need to expose jQuery as global variable
 window.jQuery = window.$ = $;
-
 import tether from 'tether';
 global.Tether = tether;
-
-// ES6 import does not work it throws error: Missing jQuery
-// using Node.js style import works without problems
 require('bootstrap');
 
 class App extends Component {
