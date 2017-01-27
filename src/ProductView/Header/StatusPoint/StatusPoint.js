dc59c6a11fa41d3bbd469766a983ec4932357876
Christian Friedow <christian@friedow.com>
2017-01-27 11:44:45 +0100

    Status point css class based on product status

--- src/ProductView/Header/StatusPoint/StatusPoint.js
+++ src/ProductView/Header/StatusPoint/StatusPoint.js
@@ -4,7 +4,7 @@ import './StatusPoint.css';
 class StatusPoint extends Component {
     render() {
         return (
-            <div className="status-point d-flex align-items-center">
+            <div className={ `status-point d-flex align-items-center ${this.props.product.state.toLowerCase()}` }>
                 <svg width="40" height="40" viewBox="0 0 42 42">
                     <circle cx="21" cy="21" r="21" fill="rgba(0, 0, 0, 0.1)" />
                     <circle cx="20" cy="20" r="20" fill="#FFAA00" />
