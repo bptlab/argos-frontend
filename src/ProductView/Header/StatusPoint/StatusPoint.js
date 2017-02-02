92b9bc84d2f54c72f889c365ba9a9e6beb490488
Christian Friedow <christian@friedow.com>
2017-02-02 10:19:02 +0100

    Status point shadow

--- src/ProductView/Header/StatusPoint/StatusPoint.js
+++ src/ProductView/Header/StatusPoint/StatusPoint.js
@@ -6,8 +6,8 @@ class StatusPoint extends Component {
         return (
             <div className={ `status-point d-flex align-items-center ${this.props.product.state.toLowerCase()}` }>
                 <svg width="40" height="40" viewBox="0 0 42 42">
-                    <circle cx="21" cy="21" r="21" fill="rgba(0, 0, 0, 0.1)" />
-                    <circle cx="20" cy="20" r="20" />
+                    <circle cx="21" cy="21" r="21" className="status-point-shadow"/>
+                    <circle cx="20" cy="20" r="20" className="status-point"/>
                 </svg>
             </div>
         );
