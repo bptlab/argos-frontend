30c05a06698ce6cffc8d6740fd20bd34021869b4
Christian Friedow <christian@friedow.com>
2017-02-09 17:59:34 +0100

    Added webpack sass support

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -109,7 +109,7 @@ module.exports = {
         exclude: [
           /\.html$/,
           /\.(js|jsx)$/,
-          /\.css$/,
+          /\.(css|scss)$/,
           /\.json$/,
           /\.svg$/
         ],
@@ -131,6 +131,11 @@ module.exports = {
           // directory for faster rebuilds.
           cacheDirectory: true
         }
+      },
+      {
+        test: /\.scss$/,
+        include: paths.appSrc,
+        loaders: ["style", "css", "sass"]
       },
       // "postcss" loader applies autoprefixer to our CSS.
       // "css" loader resolves paths in CSS and adds assets as dependencies.
