e1058ba59c9747c19b3914e73c52f94027c253f3
Christian Friedow <christian@friedow.com>
2017-02-16 14:29:31 +0100

    Moved ProductView tests to subfolder

--- src/__tests__/ProductView/ProductView.js
+++ src/__tests__/ProductView/ProductView.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ProductView from '../ProductView/ProductView.js';
+import ProductView from '../../ProductView/ProductView.js';
 import renderer from 'react-test-renderer';
-import TestProduct from './testData/product.js'
-import TestEventTypes from './testData/eventTypes.js'
-import TestEvents from './testData/events.js'
+import TestProduct from './../testData/product.js'
+import TestEventTypes from './../testData/eventTypes.js'
+import TestEvents from './../testData/events.js'
 let instance;
 
 test("Correct drawing of ProductView", () => {
