6cf919a567f9684294d6cd9cab00e4ff325c1638
Christian Friedow <christian@friedow.com>
2017-02-16 17:41:08 +0100

    Tested onChangeFilterInput

--- src/__tests__/ProductView/ProductView.js
+++ src/__tests__/ProductView/ProductView.js
@@ -7,17 +7,40 @@ import TestEvents from './../testData/events.js'
 let instance;
 
 test("Rendering of ProductView", () => {
+    let mockCallback = jest.fn();
     const component = renderer.create(
         <ProductView
             ref={(child) => {instance = child}}
-            dataSource={{fetchProduct: function() {return true;}, fetchEventTypesOf: function() {return true;}, fetchEventsOf: function() {return true;}}}
+            dataSource={{fetchProduct: mockCallback, fetchEventTypesOf: mockCallback, fetchEventsOf: mockCallback}}
             params={{productId: 0}}/>
     );
     instance.handleProductData(TestProduct.PRODUCT);
     instance.handleEventTypeData(TestEventTypes.EVENTTYPES);
-    instance.activeEventType = TestEventTypes.EVENTTYPES[0];
+    instance.loadEventsFor(TestEventTypes.EVENTTYPES[0]);
     instance.handleEventData(TestEvents.EVENTS);
 
-    const tree = component.toJSON();
+    expect(mockCallback).toHaveBeenCalledTimes(3);
+
+    let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+
+    instance.handleError(404);
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('Changing filter input', () => {
+    let mockCallback = jest.fn();
+    renderer.create(
+        <ProductView
+            ref={(child) => {instance = child}}
+            dataSource={{fetchProduct: mockCallback, fetchEventTypesOf: mockCallback, fetchEventsOf: mockCallback}}
+            params={{productId: 0}}/>
+    );
+    expect(instance.state.filter).toHaveLength(1);
+    instance.onChangeFilterInput(0, 'Query');
+    expect(instance.state.filter).toHaveLength(2);
+    expect(instance.state.filter[0].value).toMatch('Query');
+    instance.onChangeFilterInput(0, 'NextQuery');
+    expect(instance.state.filter[0].value).toMatch('NextQuery');
 });
