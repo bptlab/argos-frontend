4706f6b7c0d31ec475e1c3e4309f27503212ce80
Tom Martensen <tom.martensen@student.hpi.uni-potsdam.de>
2017-02-23 14:14:38 +0100

    use correct env variable

--- scripts/sonarqube.sh
+++ scripts/sonarqube.sh
@@ -7,7 +7,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo "normal commit"
     sonar-scanner \
               -Dsonar.host.url=$SONAR_HOST_URL \
-              -Dsonar.login=$SONARQUBE_AUTH_TOKEN \
+              -Dsonar.login=$SONAR_AUTH_TOKEN \
               -Dsonar.branch=$TRAVIS_BRANCH
 
 else
@@ -17,7 +17,7 @@ else
                   -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
                   -Dsonar.github.oauth=$GITHUB_ACCESS_TOKEN \
                   -Dsonar.host.url=$SONAR_HOST_URL \
-                  -Dsonar.login=$SONARQUBE_AUTH_TOKEN \
+                  -Dsonar.login=$SONAR_AUTH_TOKEN \
                   -Dsonar.branch=$TRAVIS_BRANCH
 
 fi
