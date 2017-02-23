5be2708e57db4312f2e2cca6c8acc572f135cdae
Christian Friedow <christian@friedow.com>
2017-02-23 16:47:46 +0100

    Added names and types to Attribute inputs

--- src/ProductView/QueryInterface/QueryInterfaceHeader/QueryInterfaceHeaderAttribute/QueryInterfaceHeaderAttribute.js
+++ src/ProductView/QueryInterface/QueryInterfaceHeader/QueryInterfaceHeaderAttribute/QueryInterfaceHeaderAttribute.js
@@ -8,11 +8,11 @@ class QueryInterfaceHeaderAttribute extends Component {
     }
 
     onChangeAttributeName (event) {
-
+        this.props.onChangeAttributeName(this.props.eventTypeAttribute.id, event.target.value);
     }
 
     onChangeAttributeType (event) {
-
+        this.props.onChangeAttributeType(this.props.eventTypeAttribute.id, event.target.value);
     }
 
     render() {
@@ -20,11 +20,11 @@ class QueryInterfaceHeaderAttribute extends Component {
             <div className="query-interface-header-attribute form-group row">
                 <div className="col-6">
                     <input className="form-control" type="text" placeholder="Attribute name" id="example-text-input"
-                           value={eventTypeAttribute}
+                           value={this.props.eventTypeAttribute.name}
                            onChange={this.onChangeAttributeName}/>
                 </div>
                 <div className="col-6">
-                    <select className="form-control" id="exampleSelect1">
+                    <select className="form-control" id="exampleSelect1" value={this.props.eventTypeAttribute.type} onChange={this.onChangeAttributeType}>
                         <option>String</option>
                         <option>Boolean</option>
                         <option>Integer</option>
