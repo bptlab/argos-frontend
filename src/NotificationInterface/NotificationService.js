042cab7b245ac2c2643e47efd81109d9458555e3
Julian <julian.weise@student.hpi.uni-potsdam.de>
2017-02-23 14:31:48 +0100

    Code refactoring NotificationService

--- src/NotificationInterface/NotificationService.js
+++ src/NotificationInterface/NotificationService.js
@@ -1,33 +1,66 @@
-class NotificationInterface {
+class NotificationService {
+
+    constructor(remoteDomain, remotePort, API, notificationCallback) {
+        this.connection = new WebSocket("ws://"+remoteDomain+":"+remotePort+"/"+API);
+        this.notificationCallback = notificationCallback;
+        this.connection.onopen = this.onOpenConnection.bind(this);
+        this.connection.onclose = this.onCloseConnection.bind(this);
+        this.connection.onerror = this.onError.bind(this);
+        this.connection.onmessage = this.processNewMessage.bind(this);
+        this.notificationList = [];
+    }
 
     onOpenConnection() {
-        this.notificationCallback("Successfully connected to Push-Notification-Service");
+        this.notificationCallback({
+            type: "info",
+            message: "Successfully connected to Push-Notification-Service"
+        });
     }
     
     onCloseConnection() {
-        this.notificationCallback("Closed connection to Push-Notification-Service");
+        this.notificationCallback({
+            type: "danger",
+            message: "Connection to Push-Notification-Service lost / not possible"
+        });
     }
     
     onError(error) {
-        this.notificationCallback("Error on Push-Notification-Service: "+error);
+        this.notificationCallback({
+            type: "danger",
+            message: "Error on Push-Notification-Service: "+error
+        });
     }
     
     processNewMessage(event) {
         const message = JSON.parse(event.data);
-        const affectedEntity = message.entityType;
-        switch (affectedEntity) {
-            case "Product": {
-                
+        const affectedEntity = message[0].entityType;
+        const affectedSubscribers = this.notificationList.filter(function(subscriber) {
+            if(subscriber.entityOfInterest === affectedEntity) {
+                return subscriber;
             }
+        }.bind(affectedEntity));
+        affectedSubscribers.forEach(function(subscriber) {
+            subscriber.notificationCallback();
+        });
+    }
+
+    subscribe(entityType, notificationCallback) {
+        const listElement = NotificationService.buildNotificationElement(entityType, notificationCallback);
+        this.notificationList.push(listElement);
+    }
+
+    unsubscribe(entityType, notificationCallback) {
+        const listElement = NotificationService.buildNotificationElement(entityType, notificationCallback);
+        const outdatedIndex = this.notificationList.indexOf(listElement);
+        if(outdatedIndex > -1) {
+            this.notificationList.slice(outdatedIndex, 1);
         }
     }
     
-    constructor(remoteDomain, remotePort, API, notificationCallback) {
-        this.connection = new WebSocket("ws://"+remoteDomain+":"+remotePort+"/"+API);
-        this.notificationCallback = notificationCallback;
-        this.connection.onopen = this.onOpenConnection;
-        this.connection.onclose = this.onCloseConnection;
-        this.connection.onerror = this.onError;
-        this.connection.onmessage = this.processNewMessage;
+    static buildNotificationElement(entityType, notificationCallback) {
+        return ({
+            entityOfInterest: entityType,
+            notificationCallback: notificationCallback
+        });
     }
-}
+} export default NotificationService;
