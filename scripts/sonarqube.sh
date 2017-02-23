26e462e9865b6e6917bf99118ee0dbaf47829ba7
Tom Martensen <tom.martensen@student.hpi.uni-potsdam.de>
2017-02-23 13:57:06 +0100

    notification should only be sent if pull request

--- scripts/sonarqube.sh
+++ scripts/sonarqube.sh
@@ -1,14 +1,14 @@
 /usr/bin/env sh
 
-if ["$TRAVIS_PULL_REQUEST" == "false"]; then
-    sonar-scanner -Dsonar.analysis.mode=preview \
-              -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-              -Dsonar.github.oauth=$GITHUB_ACCESS_TOKEN \
+if ["$TRAVIS_PULL_REQUEST" == "false"] then
+    echo "Sonarqube analysis triggered for normal commit"
+    sonar-scanner \
               -Dsonar.host.url=$SONAR_HOST_URL \
               -Dsonar.login=$SONARQUBE_AUTH_TOKEN \
               -Dsonar.branch=$TRAVIS_BRANCH
 
 else
+    echo "Sonarqube analysis triggered for pull request"
     sonar-scanner -Dsonar.analysis.mode=preview \
                   -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
                   -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
