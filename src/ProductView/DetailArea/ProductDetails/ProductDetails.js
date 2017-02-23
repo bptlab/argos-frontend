ce4115a26036ad69c675e3d21f398121bc67100e
Julian <julian.weise@student.hpi.uni-potsdam.de>
2017-02-23 14:33:55 +0100

    Introduced new state
    Integrated new possible state of products into the Dashboard and Product-View.

--- src/ProductView/DetailArea/ProductDetails/ProductDetails.js
+++ src/ProductView/DetailArea/ProductDetails/ProductDetails.js
@@ -9,11 +9,11 @@ class ProductDetails extends Component {
                 </div>
                 <div className="row">
                     <p className="col-4">Marke:</p>
-                    <p className="col">Testbrand1</p>
+                    <p className="col">{this.props.product.family.brand}</p>
                 </div>
                 <div className="row">
                     <p className="col-4">Produktfamilie:</p>
-                    <p className="col">{this.props.product.name}</p>
+                    <p className="col">{this.props.product.family.name}</p>
                 </div>
                 <div className="row">
                     <p className="col-4">Seriennummer:</p>
