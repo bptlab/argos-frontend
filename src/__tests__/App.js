6d431ef8eaa6c50f3cd5a6ae84cbfaabf867e170
Julian <julian.weise@student.hpi.uni-potsdam.de>
2017-02-24 11:14:46 +0100

    Adapted test of App.js

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App/App.js';
+import renderer from 'react-test-renderer';
 
+let instance;
 test('renders without crashing', () => {
   const notificationCallback = jest.fn();
   //Complex parameter necessary due to React-Router nesting
@@ -10,6 +12,15 @@ test('renders without crashing', () => {
           registerForNotificationsCallback: notificationCallback
       }
   };
-  const div = document.createElement('div');
-  ReactDOM.render(<App route={props} />, div);
+  const component = renderer.create(
+      <App ref={(child) => {instance = child}}
+           route={props} />);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+  instance.notificationHandler({
+      type: "danger",
+      message: "Test-Notification"
+  });
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
