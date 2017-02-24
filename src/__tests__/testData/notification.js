bf7530d4830ab04803ff197f2c42868bf9d622f2
Julian <julian.weise@student.hpi.uni-potsdam.de>
2017-02-24 12:20:56 +0100

    Added tests for NotificationService

---
+++ src/__tests__/testData/notification.js
@@ -0,0 +1,10 @@
+const notification = [{
+    "updateReason":"UPDATE",
+    "entityType":"Event",
+    "entityId":50,
+    "dataFetchUri":"/api/products/50"
+}];
+
+export default {
+    EVENTNOTIFICATION: notification
+}
