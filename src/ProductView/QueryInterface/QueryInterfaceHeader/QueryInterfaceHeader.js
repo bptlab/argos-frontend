53e6086252be4b1afeb9022b911d02f60bef56f9
Christian Friedow <christian@friedow.com>
2017-02-27 16:26:03 +0100

    Made EventTypeName changeable

--- src/ProductView/QueryInterface/QueryInterfaceHeader/QueryInterfaceHeader.js
+++ src/ProductView/QueryInterface/QueryInterfaceHeader/QueryInterfaceHeader.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import QueryInterfaceHeaderAttribute from './QueryInterfaceHeaderAttribute/QueryInterfaceHeaderAttribute.js';
 
 class QueryInterfaceHeader extends Component {
+    constructor(props) {
+        super(props);
+        this.onChangeEventTypeName = this.onChangeEventTypeName.bind(this);
+    }
+
+    onChangeEventTypeName (event) {
+        this.props.onChangeEventTypeName(event.target.value);
+    }
+
     render() {
         return (
             <div className="query-interface-header">
                 <div className="form-group">
-                    <input className="form-control" type="text" placeholder="Name" id="query-name"/>
+                    <input className="form-control" type="text"
+                           placeholder="Name" id="query-name"
+                           value={this.props.eventTypeName}
+                           onChange={this.onChangeEventTypeName}/>
                 </div>
                 {this.props.eventTypeAttributes.map((eventTypeAttribute) =>
                     <QueryInterfaceHeaderAttribute key={eventTypeAttribute.id}
