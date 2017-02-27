76e3d51caa63fd29e3edf172bb0421f185db2e14
Julian <julian.weise@student.hpi.uni-potsdam.de>
2017-02-27 11:33:00 +0100

    Introduced centralized config to all DashboardView components.

--- src/DashboardView/Header/Header.js
+++ src/DashboardView/Header/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Boiler from '../../assets/icons/boiler.svg';
+import {argosConfig} from '../../config/argosConfig.js';
 
 class Header extends Component {
     render() {
@@ -13,7 +14,7 @@ class Header extends Component {
                             </a>
                         </div>
                         <div className="col-xs-12 col-sm-10 d-flex justify-content-around">
-                            <h1 className="dashboard-title">Early Warning Dashboard</h1>
+                            <h1 className="dashboard-title">{argosConfig.dashboardName}</h1>
                         </div>
                     </div>
                 </div>
