a39134dd8a6e0bf03067f1345d5d448ffcccb3e5
Christian Friedow <christian@friedow.com>
2017-02-28 16:12:03 +0100

    Tested QueryInterfaceHeaderAttribute

---
+++ src/__tests__/ProductView/QueryInterfaceHeaderAttribute.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import QueryInterfaceHeaderAttribute from '../../ProductView/TabBar/QueryInterface/QueryInterfaceHeader/QueryInterfaceHeaderAttribute/QueryInterfaceHeaderAttribute.js';
+import renderer from 'react-test-renderer';
+let instance;
+
+test('On change attribute name callback test', () => {
+    const callbackMock = jest.fn();
+    const eventTypeAttribute = {id: 0};
+    renderer.create(
+        <QueryInterfaceHeaderAttribute
+            ref={(child) => {instance = child}}
+            eventTypeAttribute={eventTypeAttribute}
+            onChangeAttributeName={callbackMock}/>
+    );
+    const event = {target: {value: 'test'}};
+    instance.onChangeAttributeName(event);
+    expect(callbackMock).toBeCalledWith(0, 'test');
+});
+
+test('On change attribute type callback test', () => {
+    const callbackMock = jest.fn();
+    const eventTypeAttribute = {id: 0};
+    renderer.create(
+        <QueryInterfaceHeaderAttribute
+            ref={(child) => {instance = child}}
+            eventTypeAttribute={eventTypeAttribute}
+            onChangeAttributeType={callbackMock}/>
+    );
+    const event = {target: {value: 'INTEGER'}};
+    instance.onChangeAttributeType(event);
+    expect(callbackMock).toBeCalledWith(0, 'INTEGER');
+});
