7c1cfb0f1c74a51b6e40caf221e96813a50787b4
Christian Friedow <christian@friedow.com>
2017-02-28 14:37:14 +0100

    Tested QueryInterface

--- src/__tests__/ProductView/QueryInterface.js
+++ src/__tests__/ProductView/QueryInterface.js
@@ -17,17 +17,54 @@ test('Handling change query', () => {
     expect(tree).toMatchSnapshot();
 });
 
-test('Handling save query', () => {
-    const callbackMock = jest.fn();
+test('Handling change attribute type', () => {
     renderer.create(
         <QueryInterface
             ref={(child) => {instance = child}}
             defaultQuery=""/>
     );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-    const event = {target: {value: 'TEST'}};
-    instance.handleChangeQuery(event);
-    tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    instance.handleChangeEventTypeName('Test');
+    expect(instance.state.eventTypeName).toBe('Test');
+
+});
+
+test('Handling change attribute type', () => {
+    renderer.create(
+        <QueryInterface
+            ref={(child) => {instance = child}}
+            defaultQuery=""/>
+    );
+    instance.handleChangeAttributeName(0, 'Test');
+    expect(instance.state.eventTypeAttributes[0].name).not.toBe('Test');
+    instance.handleChangeAttributeName(2, 'Test');
+    expect(instance.getEventTypeAttribute(2).name).toBe('Test');
+    expect(instance.state.eventTypeAttributes.length).toBe(4);
+    instance.handleChangeAttributeName(2, '');
+    expect(instance.state.eventTypeAttributes.length).toBe(3);
+});
+
+test('Handling change attribute type', () => {
+    renderer.create(
+        <QueryInterface
+            ref={(child) => {instance = child}}
+            defaultQuery=""/>
+    );
+    instance.handleChangeAttributeType(0, 'STRING');
+    expect(instance.state.eventTypeAttributes[0].type).not.toBe('STRING');
+    instance.handleChangeAttributeType(2, 'INTEGER');
+    expect(instance.state.eventTypeAttributes[2].type).toBe('INTEGER');
+
+});
+
+test('Handling save query', () => {
+    const callbackMock = jest.fn();
+    const dataSender = {'createEventtype': callbackMock};
+    renderer.create(
+        <QueryInterface
+            ref={(child) => {instance = child}}
+            dataSender={dataSender}
+            defaultQuery=""/>
+    );
+    instance.handleSaveQuery();
+    expect(callbackMock).toBeCalled();
 });
