1f273ccfc65a1636092adbb7383415fe198e7cfd
Christian Friedow <christian@friedow.com>
2017-03-23 12:04:33 +0100

    Refactored tests

--- src/__tests__/ProductView/QueryInterfaceAttribute.js
+++ src/__tests__/ProductView/QueryInterfaceAttribute.js
@@ -1,31 +1,28 @@
 import React from 'react';
 import QueryInterfaceAttribute from '../../ProductView/TabBar/QueryInterface/QueryInterfaceAttributeList/QueryInterfaceAttribute/QueryInterfaceAttribute.js';
 import renderer from 'react-test-renderer';
-let instance;
 
-test('On change attribute name callback test', () => {
-    const callbackMock = jest.fn();
+let instance;
+const callbackMock = jest.fn();
+
+beforeEach(() => {
     const eventTypeAttribute = {id: 0};
     renderer.create(
-        <QueryInterfaceHeaderAttribute
+        <QueryInterfaceAttribute
             ref={(child) => {instance = child}}
             eventTypeAttribute={eventTypeAttribute}
-            onChangeAttributeName={callbackMock}/>
+            onChangeAttributeName={callbackMock}
+            onChangeAttributeType={callbackMock}/>
     );
+});
+
+test('On change attribute name callback test', () => {
     const event = {target: {value: 'test'}};
     instance.onChangeAttributeName(event);
     expect(callbackMock).toBeCalledWith(0, 'test');
 });
 
 test('On change attribute type callback test', () => {
-    const callbackMock = jest.fn();
-    const eventTypeAttribute = {id: 0};
-    renderer.create(
-        <QueryInterfaceAttribute
-            ref={(child) => {instance = child}}
-            eventTypeAttribute={eventTypeAttribute}
-            onChangeAttributeType={callbackMock}/>
-    );
     const event = {target: {value: 'INTEGER'}};
     instance.onChangeAttributeType(event);
     expect(callbackMock).toBeCalledWith(0, 'INTEGER');
