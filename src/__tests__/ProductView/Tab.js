5c4d4f00a2f10b7bfb30e3de6d0c5fea6759239b
Julian <julian.weise@student.hpi.uni-potsdam.de>
2017-03-23 10:36:11 +0100

    Adapted test to previous changes

--- src/__tests__/ProductView/Tab.js
+++ src/__tests__/ProductView/Tab.js
@@ -5,21 +5,13 @@ let instance;
 
 test('Clicking ProductView Tab', () => {
     const setActiveEventType = jest.fn();
-    const notificationService = {
-        subscribe: jest.fn(),
-        unsubscribe: jest.fn()
-    };
     renderer.create(
         <Tab ref={(child) => {instance = child}} 
              eventType={{}}
              setActiveEventType={setActiveEventType} 
              product={{}}
-             notificationService={notificationService}
             />
     );
     instance.setActiveEventType();
-    instance.componentWillUnmount();
     expect(setActiveEventType).toBeCalled();
-    expect(notificationService.subscribe).toBeCalled();
-    expect(notificationService.unsubscribe).toBeCalled();
 });
