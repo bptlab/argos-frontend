c34569abad8402776903a64d886fd27de752b513
Maximilian <maximilian.voelker@student.hpi.de>
2017-03-28 13:06:40 +0200

    move configuration data mock up to productview

--- src/ProductView/Header/Header.js
+++ src/ProductView/Header/Header.js
@@ -4,12 +4,6 @@ import ConfigurationHeader from './ConfigurationHeader/ConfigurationHeader.js';
 
 class Header extends Component {
     render() {
-        let test = {
-            "1.0":[ "4.0.0", "4.0.1", "4.0.2" ],
-            "1.1":[ "4.1.0", "4.1.1", "4.1.2" ],
-            "1.2":[ "4.2.0", "4.2.1", "4.2.2" ],
-            "2.0":[ "5.0.0", "5.0.1", "5.0.2" ]
-        };
         return (
             <header>
                 <div className="container">
