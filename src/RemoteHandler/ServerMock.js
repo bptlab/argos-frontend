bb4bbc2bb6505b382d7a1011dc07ae8f0ce5e212
Julian <julian.weise@student.hpi.uni-potsdam.de>
2017-03-30 12:46:18 +0200

    Added SeverMock Data and testData for configurations

--- src/RemoteHandler/ServerMock.js
+++ src/RemoteHandler/ServerMock.js
@@ -23,6 +23,36 @@ class ServerMock {
             value: value
         });
     }
+    
+    static getProductConfigurations() {
+        return [{
+            id: 0,
+            productId: 0,
+            state: "RUNNING",
+            stateDescription: "No errors or warnings detected",
+            codingPlugId: 30,
+            codingPlugSoftwareVersions: [1.0, 1.1, 3.1],
+            numberOfEvents: 234,
+            statusUpdateQueries: {
+                RUNNING: "ESPER-QUERY",
+                WARNING: "ESPER-QUERY",
+                ERROR: "ESPER-QUERY"
+            },
+            errorTypes: [{
+                id: 0,
+                errorTypeId: "0",
+                displayCode: "0Z",
+                causeCode:  0,
+                errorDescription: "Heating produces strange sounds",
+                errorCauses: [{
+                    id: 0,
+                    causeDescription: "Kabelbaum missing",
+                    errorOccurrences: 3,
+                    errorPrediction: 0.3
+                }]
+            }]
+        }];
+    }
 
     static getProductFamily() {
         return [{
