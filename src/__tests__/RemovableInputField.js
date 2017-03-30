964bc9efc4d7756b3e7810189d88ab65f8f65e0f
Frederike Ramin <frederike.ramin@student.hpi.de>
2017-03-30 16:30:59 +0200

    Test remove button for filters

--- src/__tests__/RemovableInputField.js
+++ src/__tests__/RemovableInputField.js
@@ -3,12 +3,25 @@ import RemovableInputField from '../Utils/RemovableInputField.js';
 import renderer from 'react-test-renderer';
 let instance;
 
+test("Rendering of RemovableInputField with button", () => {
+    const callbackMock = jest.fn();
+    const component = renderer.create(
+        <RemovableInputField ref={(child) => {instance = child}}
+                             id='0' value=''
+                             onInputChange={callbackMock}
+                             showRemove={true}/>
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
 test('Handling ProductView Filter input', () => {
     const callbackMock = jest.fn();
     renderer.create(
         <RemovableInputField ref={(child) => {instance = child}}
                              id='0' value=''
-                             onInputChange={callbackMock}/>
+                             onInputChange={callbackMock}
+                             showRemove={false}/>
     );
     const event = {target: {}};
     instance.handleInput(event);
