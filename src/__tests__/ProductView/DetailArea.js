1844aba27ed10d6f663963073a2a3e208f9d6f3f
Christian Friedow <christian@friedow.com>
2017-04-03 14:31:17 +0200

    Fixed quality gate

--- src/__tests__/ProductView/DetailArea.js
+++ src/__tests__/ProductView/DetailArea.js
@@ -5,23 +5,25 @@ import react from 'react-dom';
 import testProduct from '../testData/product.js';
 import testConfigurations from '../testData/frontend_configurations.js';
 let instance;
-let node = document.createElement('div');
+const node = document.createElement('div');
 
 test('Render DetailArea', () => {
-    let component = react.render(<DetailArea ref={(child) => {instance = child}}
-        product={testProduct.PRODUCT}
-        configuration={testConfigurations.CONFIGURATIONS[0]}
-        showAllConfigurations={true}
-        />, node);
+    react.render(
+        <DetailArea ref={(child) => {instance = child}}
+            product={testProduct.PRODUCT}
+            configuration={testConfigurations.CONFIGURATIONS[0]}
+            showAllConfigurations={true}/>, 
+        node);
     expect(instance.state.configuration).toEqual(testProduct.PRODUCT);
 });
 
 
 test('Show configuration-details', () => {
-    let component = react.render(<DetailArea ref={(child) => {instance = child}}
-        product={testProduct.PRODUCT}
-        configuration={testConfigurations.CONFIGURATIONS[0]}
-        showAllConfigurations={false}
-        />, node);
+    react.render(
+        <DetailArea ref={(child) => {instance = child}}
+            product={testProduct.PRODUCT}
+            configuration={testConfigurations.CONFIGURATIONS[0]}
+            showAllConfigurations={false}/>, 
+        node);
     expect(instance.state.configuration).toEqual(testConfigurations.CONFIGURATIONS[0]);
 })
