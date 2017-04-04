cd764cad76091ce713e28534345dd9ac52a99646
Tom Martensen <tom.martensen@student.hpi.de>
2017-04-04 10:56:00 +0200

    updates default event query

--- src/config/argosConfig_template.js
+++ src/config/argosConfig_template.js
@@ -25,7 +25,7 @@ export const argosConfig = {
     errorRemainingDependencies:     'You can not delete this Event-Type, because of existing dependencies',
     messageSuccessEventTypeCreation:'EventType sucessfully created.',
     createEventTypeDefaultQuery:    'INSERT INTO TestErrorEvents SELECT timestamp, productId, ' +
-                                    'productFamilyId FROM FeedbackData',
+                                    'productFamilyId, codingPlugId, codingPlugSoftwareVersionu FROM FeedbackData',
     formValidationNoEmptyMessage:   'This field can\'t be empty.',
     runningStateName:               'running',
     warningStateName:               'warning',
