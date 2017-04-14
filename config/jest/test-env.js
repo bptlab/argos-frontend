9b80d80ac9b90a738981016f9dc771fc2ea9d25a
Christian Friedow <christian@friedow.com>
2017-04-14 10:31:32 +0200

    Initialized a new react app

--- config/jest/test-env.js
+++
@@ -1,4 +0,0 @@
-import $ from 'jquery';
-import Tether from 'tether';
-global.$ = global.jquery = global.jQuery = window.jQuery = global.window.jquery = $;
-global.Tether = global.tether = Tether;
--- src/Loader/Loader.js
+++
@@ -1,8 +0,0 @@
-import React, {Component} from 'react';
-class Loader extends Component {
-    render() {
-        return(
-            <div className="loader"></div>
-        );
-    }
-} export default Loader;
