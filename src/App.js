c564a483026d4309e36f26b827bef328bf1f1b01
Christian Friedow <christian@friedow.com>
2017-04-16 18:06:11 +0200

    Included header and added css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Header from './Header';
 import './App.css';
 
 class App extends Component {
     render() {
         return (
             <MuiThemeProvider>
-
+                <Header />
             </MuiThemeProvider>
         );
     }
