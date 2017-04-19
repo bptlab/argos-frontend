383e492e1e50cc93f72e0636e58fa3bdd1703674
Christian Friedow <christian@friedow.com>
2017-04-19 11:46:43 +0200

    Moved GridView into a own directory

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import ActionHome from 'material-ui/svg-icons/action/home';
+import Settings from 'material-ui/svg-icons/action/settings';
 
 class Header extends Component {
 	render() {
 		return (
 			<AppBar
-				title="Argos"
-				showMenuIconButton={false}
-				iconClassNameRight="muidocs-icon-navigation-expand-more"
+				title={<span>Argos</span>}
+				iconElementLeft={<IconButton><ActionHome/></IconButton>}
+				iconElementRight={<IconButton><Settings/></IconButton>}
 			/>
 		);
 	}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import GridView from './GridView';
+import GridView from './GridView/GridView';
 import './index.css';
 
 ReactDOM.render(
