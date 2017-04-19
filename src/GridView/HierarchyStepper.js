34bd833c07f9df85ff17ad518e5e3e5a32ea6200
Christian Friedow <christian@friedow.com>
2017-04-19 12:45:43 +0200

    Changed icon in stepper

--- src/GridView/HierarchyStepper.js
+++ src/GridView/HierarchyStepper.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Step, Stepper, StepLabel } from 'material-ui/Stepper';
+import ArrowForwardIcon from 'material-ui/svg-icons/communication/call-split';
 
 
 class HierarchyStepper extends Component {
 	render() {
 		return (
 			<div>
-				<Stepper activeStep={0}>
+				<Stepper activeStep={3} connector={<ArrowForwardIcon className="rotate-90" />}>
 					<Step>
 						<StepLabel>Productfamilies</StepLabel>
 					</Step>
@@ -15,7 +16,7 @@ class HierarchyStepper extends Component {
 					</Step>
 					<Step>
 						<StepLabel>Configurations</StepLabel>
-						<StepLabel>Parts</StepLabel>
+						<StepLabel completed={false}>Parts</StepLabel>
 					</Step>
 				</Stepper>
 			</div>
