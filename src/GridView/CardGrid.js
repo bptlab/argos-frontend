090860866527c9bb51bf59df8e849c8764bd6b35
Julian <julian.weise@student.hpi.uni-potsdam.de>
2017-04-25 13:46:27 +0200

    Fixed error of synchron data fetching

--- src/GridView/CardGrid.js
+++ src/GridView/CardGrid.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import {connect, PromiseState} from 'react-refetch';
+import {connect} from 'react-refetch';
 import { Card, CardTitle, CardText, CardActions } from 'material-ui/Card';
+import LoadingAnimation from './../Utils/LoadingAnimation';
+import ErrorMessage from './../Utils/ErrorMessage';
 import FlatButton from 'material-ui/FlatButton';
 import './CardGrid.css';
 
@@ -11,31 +13,37 @@ class CardGrid extends Component {
 		const attributeNames = props.entity.Attributes.map((attribute) => {
 			return attribute.Name;
 		});
-		this.attributesList = attributeNames.join("+");
+		this.props.lazyChildEntities(attributeNames.join("+"));
 	}
 	
 	render() {
-		return (
-			<div className="card-grid d-flex">
-				{this.props.lazyChildEntities(this.attributesList).map((childEntity) => {
-					return (
-						<Card className="card">
-							<CardTitle title={childEntity.Name} subtitle={this.props.childEntityType.name} />
-							<CardText>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-								Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-								Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-								Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-							</CardText>
-							<CardActions className="d-flex">
-								<FlatButton label="Children" className="card-button"/>
-								<FlatButton label="Inspect" className="card-button"/>
-							</CardActions>
-						</Card>
-					);
-				})}
-			</div>
-		);
+		if (this.props.childEntities.pending) {
+			return <LoadingAnimation/>;
+		} else if (this.props.childEntities.rejected) {
+			return <ErrorMessage message={this.props.childEntities.reason}/>;
+		} else if (this.props.childEntities.fulfilled) {
+			return (
+				<div className="card-grid d-flex">
+					{this.props.childEntities.map((childEntity) => {
+						return (
+							<Card className="card">
+								<CardTitle title={childEntity.Name} subtitle={this.props.childEntityType.name}/>
+								<CardText>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+									Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+									Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+									Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+								</CardText>
+								<CardActions className="d-flex">
+									<FlatButton label="Children" className="card-button"/>
+									<FlatButton label="Inspect" className="card-button"/>
+								</CardActions>
+							</Card>
+						);
+					})}
+				</div>
+			);
+		}
 	}
 }
 
