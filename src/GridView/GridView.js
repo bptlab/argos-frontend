952b13fa6ada29d68ad737aefcac3aa87276c04b
Julian <julian.weise@student.hpi.uni-potsdam.de>
2017-04-25 10:51:39 +0200

    Implemented data fetching for GridView

--- src/GridView/GridView.js
+++ src/GridView/GridView.js
@@ -1,32 +1,36 @@
-import React, { Component } from 'react';
-import DonutChart from './DonutChart'
-import HierarchyStepper from './HierarchyStepper'
-import SearchBar from './../Utils/SearchBar'
+import React, {Component} from 'react';
+import DonutChart from './DonutChart';
+import HierarchyStepper from './HierarchyStepper';
+import SearchBar from './../Utils/SearchBar';
 import CardGrid from './CardGrid';
+import {connect, PromiseState} from 'react-refetch';
 
 class GridView extends Component {
-
-
-	componentDidMount() {
-		this.props.datasource.getEntities(3, this.getEntities);
-	}
-
-	getEntities(entities) {
-		this.setState({
-			entities: entities
-		});
-	}
-
+	
 	render() {
-		return (
-			<div>
-				<HierarchyStepper/>
-				<DonutChart/>
-				<SearchBar/>
-				<CardGrid entities={this.props.entities}/>
-			</div>
-		);
+		const { hierarchy, entity } = this.props;
+		const allFetches = PromiseState.all([hierarchy, entity]);
+		if (allFetches.pending) {
+			return <LoadingAnimation/>;
+		} else if (allFetches.rejected) {
+			return <ErrorMessage error={allFetches.reason}/>;
+		} else if (allFetches.fulfilled) {
+			return (
+				<div>
+					<h1>{entity.Name}</h1>
+					<HierarchyStepper
+						hierarchy={hierarchy}
+						currentEntityType={entity.TypeId}/>
+					<DonutChart/>
+					<SearchBar/>
+					<CardGrid currentEntityId={this.props.match.params.entityId}/>
+				</div>
+			);
+		}
 	}
 }
 
-export default GridView;
+export default connect(props => ({
+	hierarchy: `/entityType/hierarchy`,
+	entity: `/entity/${props.match.params.entityId}`
+}))(GridView);
