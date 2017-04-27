d304b21d7876d0683f3b28811823b8c33cb084c3
Tom Martensen <mail@tommartensen.de>
2017-04-27 14:18:42 +0200

    added not found view

--- src/config/config-template.js
+++ src/config/config-template.js
@@ -6,6 +6,7 @@ const config = {
 		tertiaryColor: "#001c24",
 		errorColor: "#B71C1C"
 	}
+    notFoundMessage: "Error 404. The requested page does not exist. You found a glitch in the matrix.",
 };
 
 export default config;
