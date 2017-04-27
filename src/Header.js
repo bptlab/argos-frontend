a5b1984de3fc2b152bc01c33ca4634e178dc8520
Maximilian <maximilian.voelker@student.hpi.de>
2017-04-27 08:53:17 +0200

    first try to get the current page in header

--- src/Header.js
+++ src/Header.js
@@ -3,18 +3,27 @@ import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
 import IconHome from 'material-ui/svg-icons/action/home';
 import IconSettings from 'material-ui/svg-icons/action/settings';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import config from './config/config'
 
 class Header extends Component {
 	render() {
 		return (
-			<AppBar
-				title={<span>{config.projectName}</span>}
-				iconElementLeft={<IconButton><IconHome/></IconButton>}
-				iconElementRight={<IconButton><IconSettings/></IconButton>}
-			/>
+			<Router>
+				<div>
+					<Route path="/grid/:entityId" component={() => <h2>Grid</h2>}/>
+					<Route path="/details" component={() => <h2>Detail</h2>}/>
+					<Route path="/settings" component={() => <h2>Settings</h2>}/>
+				</div>
+			</Router>
 		);
-	}
+    }
 }
-
 export default Header;
+
+/*<AppBar
+ title={<span>{config.projectName}</span>}
+ iconElementLeft={<IconButton><IconHome/></IconButton>}
+ iconElementRight={<IconButton><IconSettings/></IconButton>}
+ className="primary-color"
+ />*/
