b4d451003f79d0ab8264b6439e7aa8a4bd754c38
Christian Friedow <christian@friedow.com>
2017-04-27 16:17:42 +0200

    Added side by side styling of attributes and diagram

--- src/DetailView/DetailView.js
+++ src/DetailView/DetailView.js
@@ -4,13 +4,17 @@ import EventDiagram from './EventDiagram';
 import SearchBar from './../Utils/SearchBar';
 import EventTabs from './EventTabs';
 import EventTable from './EventTable';
+import { css } from 'aphrodite';
+import AppStyles from './../AppStyles';
 
 class DetailView extends Component {
 	render() {
 		return (
 			<div>
-				<EntityInformation/>
-				<EventDiagram/>
+				<div className={css(AppStyles.dFlex)}>
+					<EntityInformation styles={[AppStyles.w50]}/>
+					<EventDiagram styles={[AppStyles.w50]}/>
+				</div>
 				<SearchBar/>
 				<EventTabs/>
 				<EventTable/>
--- src/DetailView/EventDiagram.js
+++ src/DetailView/EventDiagram.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import { css } from 'aphrodite';
 
 class EventDiagram extends Component {
 	render() {
 		return (
-			<div>
+			<div className={css(this.props.styles)}>
 				404 Diagram not found
 			</div>
 		);
