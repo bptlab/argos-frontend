b53f8aebadd3a7294d25f1a3856b4669efaaf7a1
Christian Friedow <christian@friedow.com>
2017-04-27 09:55:47 +0200

    Renamed the view

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import GridView from './GridView/GridView';
 import DetailView from './DetailView/DetailView';
-import SettingsView from './SettingsView/SettingsView';
+import EventTypesView from './EventTypesView/EventTypesView';
 import './definitions.css';
 import './index.css';
 
@@ -13,7 +13,7 @@ ReactDOM.render(
 		<App>
 			<Route path="/grid/:entityId" component={GridView}/>
 			<Route path="/details" component={DetailView}/>
-			<Route path="/settings" component={SettingsView}/>
+			<Route path="/eventtypes" component={EventTypesView}/>
 		</App>
 	</Router>,
 	document.getElementById('root')
