6d6e60d1e4d89b99904fb4280d16b7a1c9fd955c
Maximilian <maximilian.voelker@student.hpi.de>
2017-04-27 09:10:50 +0200

    display buttons dependent on currently loaded page

--- src/Header.js
+++ src/Header.js
@@ -3,29 +3,48 @@ import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
 import IconHome from 'material-ui/svg-icons/action/home';
 import IconSettings from 'material-ui/svg-icons/action/settings';
+import IconArrowBack from 'material-ui/svg-icons/navigation/arrow-back';
+import config from './config/config';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import config from './config/config'
 import { css } from 'aphrodite';
 import DefinitionStyles from './DefinitionStyles';
 
 class Header extends Component {
+
+	composeAppBar(pageLocation) {
+		let iconElementLeft;
+
+		if (pageLocation === "grid") {
+            iconElementLeft = <IconButton><IconHome/></IconButton>;
+        }
+        if (pageLocation === "details") {
+            iconElementLeft = <IconButton><IconArrowBack/></IconButton>;
+        }
+        if (pageLocation === "settings") {
+			iconElementLeft = <IconButton><IconArrowBack/></IconButton>;
+        }
+
+        return (
+			<AppBar
+				title={<span>{config.projectName}</span>}
+				iconElementLeft={iconElementLeft}
+				iconElementRight={<IconButton><IconSettings/></IconButton>}
+				className="primary-color"
+			/>
+		);
+	}
+
 	render() {
 		return (
 			<Router>
 				<div>
-					<Route path="/grid/:entityId" component={() => <h2>Grid</h2>}/>
-					<Route path="/details" component={() => <h2>Detail</h2>}/>
-					<Route path="/settings" component={() => <h2>Settings</h2>}/>
+					<Route path="/grid/:entityId" component={() => this.composeAppBar("grid")}/>
+					<Route path="/details" component={() => this.composeAppBar("details")}/>
+					<Route path="/settings" component={() => this.composeAppBar("settings")}/>
 				</div>
 			</Router>
 		);
     }
 }
 export default Header;
-
-/*<AppBar
- title={<span>{config.projectName}</span>}
- iconElementLeft={<IconButton><IconHome/></IconButton>}
- iconElementRight={<IconButton><IconSettings/></IconButton>}
- className="primary-color"
- />*/
