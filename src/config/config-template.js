c766a45a5ebfe4aa7c65077012af0bde503b9b6e
Tom Martensen <mail@tommartensen.de>
2017-05-02 10:43:01 +0200

    fixed config-template merge

--- src/config/config-template.js
+++ src/config/config-template.js
@@ -5,7 +5,7 @@ const config = {
 		secondaryColor: "#007b9d",
 		tertiaryColor: "#001c24",
 		errorColor: "#B71C1C"
-	}
+	},
     notFoundMessage: "Error 404. The requested page does not exist. You found a glitch in the matrix.",
 };
 
