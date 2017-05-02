881a1767cc9b0c9689ff8ad7e5a6526721fdc92c
Christian Friedow <christian@friedow.com>
2017-05-02 14:17:01 +0200

    Removed unneccesary color definitions

--- src/DefinitionStyles.js
+++ src/DefinitionStyles.js
@@ -1,21 +1,6 @@
 import { StyleSheet } from 'aphrodite';
-import config from './config/config';
 
 const DefinitionStyles = StyleSheet.create({
-	// Colors //
-	primaryBackgroundColor: {
-		backgroundColor: config.colors.primaryColor,
-	},
-	secondaryBackgroundColor: {
-		backgroundColor: config.colors.secondaryColor,
-	},
-	tertiaryBackgroundColor: {
-		backgroundColor: config.colors.tertiaryColor,
-	},
-	errorColor: {
-		color: config.colors.errorColor,
-	},
-
 	// Spacing //
 	elementMarginTop: {
 		marginTop: '30px'
