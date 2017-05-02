6af977ce9a95083b501680ee260256b4a5969634
Christian Friedow <christian@friedow.com>
2017-05-02 09:13:04 +0200

    Moved color config from template to config

--- src/config/config-template.js
+++ src/config/config-template.js
@@ -1,10 +1,20 @@
 const config = {
 	projectName: "Argos",
 	colors: {
-		primaryColor: "#004E64",
-		secondaryColor: "#007b9d",
-		tertiaryColor: "#001c24",
-		errorColor: "#B71C1C"
+		primaryDark: "#000051",
+		primary: "#1a237e",
+		primaryLight: "#534bae",
+		accent: "#ffc107",
+		accentLight: "#fff350",
+		text: "#424242",
+		textAlternate: "#ffffff",
+		border: "#e0e0e0",
+		error: "#B71C1C",
+		transparent: {
+			primary: "rgba(0, 78, 100, 0.7)",
+			secondary: "rgba(0, 123, 157, 0.5)",
+			tertiary: "rgba(0, 28, 36, 0.5)",
+		}
 	}
 };
 
