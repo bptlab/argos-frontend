8a5b9b8fe66b1f4a48d5867e3692b5cd8894df73
Julian <julian.weise@student.hpi.uni-potsdam.de>
2017-05-02 10:45:53 +0200

    Added neccessary parameter radix to parseInt

--- src/Utils/MockData/BackendMock.js
+++ src/Utils/MockData/BackendMock.js
@@ -29,7 +29,7 @@ class BackendMock {
 	
 	static getEntity(params) {
 		return Entity.find((entity) => {
-			return entity.Id === parseInt(params[1]);
+			return entity.Id === parseInt(params[1], 10);
 		});
 	}
 	
@@ -39,7 +39,7 @@ class BackendMock {
 	
 	static getChildEntitiesOfEntityType(params) {
 		return Entity.filter((Entity) => {
-			return (Entity.ParentId === parseInt(params[1]) && Entity.TypeId === parseInt(params[2]));
+			return (Entity.ParentId === parseInt(params[1], 10) && Entity.TypeId === parseInt(params[2], 10));
 		});
 	}
 	
