0d4ef7ad528b98d674d44d5861cf6baa8dfca7a4
Maximilian <maximilian.voelker@student.hpi.de>
2017-05-04 08:32:01 +0200

    add mockd data for event type attributes

---
+++ src/Utils/MockData/TransportForLondon/EventTypeAttribute.js
@@ -0,0 +1,78 @@
+const EventTypeAttribute = [
+    {
+        Id: 99101991,
+        TypeAttributes: [
+            {
+                Id: 99101991991,
+                Name: "timestamp"
+            },
+            {
+                Id: 99101991992,
+                Name: "load"
+            },
+            {
+                Id: 99101991993,
+                Name: "nrOfBusses"
+            }
+        ]
+    },
+    {
+        Id: 99201991,
+        TypeAttributes: [
+            {
+                Id: 99201991991,
+                Name: "timestamp"
+            },
+            {
+                Id: 99201991992,
+                Name: "WagonsPerTrain"
+            },
+            {
+                Id: 99201991993,
+                Name: "NrOfTrains"
+            }
+        ]
+    },    {
+        Id: 99101992,
+        TypeAttributes: [
+            {
+                Id: 99101992991,
+                Name: "timestamp"
+            },
+            {
+                Id: 99101992992,
+                Name: "isOperating"
+            },
+            {
+                Id: 99101992993,
+                Name: "loadPrediction"
+            }
+        ]
+    },    {
+        Id: 991011991,
+        TypeAttributes: [
+            {
+                Id: 991011991991,
+                Name: "timestamp"
+            },
+            {
+                Id: 991011991992,
+                Name: "crowded"
+            }
+        ]
+    },    {
+        Id: 991011992,
+        TypeAttributes: [
+            {
+                Id: 991011992991,
+                Name: "timestamp"
+            },
+            {
+                Id: 991011992992,
+                Name: "interrupted"
+            }
+        ]
+    }
+];
+
+export default EventTypeAttribute;
--- src/Utils/MockData/TransportForLondon/readme.txt
+++ src/Utils/MockData/TransportForLondon/readme.txt
@@ -9,5 +9,9 @@ EventTypes:
     99*id of entity*99*counter for ET of this entity*
     id's for root entities would be 9910991, 9910992, 9920991, ...
 
+EventTypeAttributes:
+    *id of event type*99*counter of attribute of this entity*
+    First attribute of event type 9910991 would be: 9910991991
+
 
 
