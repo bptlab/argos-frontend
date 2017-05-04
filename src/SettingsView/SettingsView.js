e91af11852393ce71246a270e04cdbfbc7c0e1f2
Julian <julian.weise@student.hpi.uni-potsdam.de>
2017-05-04 09:26:40 +0200

    Fixed bug, props is required in connect, regardless of its use

--- src/SettingsView/SettingsView.js
+++ src/SettingsView/SettingsView.js
@@ -25,6 +25,6 @@ class SettingsView extends ConnectionComponent {
 	}
 }
 
-export default connect.defaults({fetch: ConnectionComponent.switchFetch})(({
+export default connect.defaults({fetch: ConnectionComponent.switchFetch})(props => ({
 	eventTypes: `/eventtypes`,
 }))(SettingsView);
