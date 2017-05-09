6329dccbec556391354c9ef71937e8024ca924c9
Tom Martensen <mail@tommartensen.de>
2017-05-09 09:47:09 +0200

    moved description onto card

--- src/CreateEventQueryView/EventTypeInformation.js
+++ src/CreateEventQueryView/EventTypeInformation.js
@@ -1,6 +1,8 @@
 import React, {Component} from "react";
 import {List, ListItem} from "material-ui/List";
 import {css} from "aphrodite";
+import {Card, CardHeader, CardText, CardTitle} from "material-ui";
+import AppStyles from "../AppStyles";
 
 class EventTypeInformation extends Component {
     static generateListItem(attribute, key) {
@@ -12,11 +14,18 @@ class EventTypeInformation extends Component {
 
     render() {
         return (
-            <List className={css(this.props.styles)}>
-                {this.props.attributes.map(
-                    (attribute, key) => EventTypeInformation.generateListItem(attribute, key)
-                )}
-            </List>
+            <Card>
+                <CardTitle className={css(AppStyles.textAlignCenter, AppStyles.fontSize24)}>
+                    Available Attributes
+                </CardTitle>
+                <CardText>
+                    <List>
+                        {this.props.attributes.map(
+                            (attribute, key) => EventTypeInformation.generateListItem(attribute, key)
+                        )}
+                    </List>
+                </CardText>
+            </Card>
         );
     }
 }
