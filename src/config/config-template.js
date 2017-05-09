c2b8188e56880bf402fd855fd011e4702b2abfb2
Tom Martensen <mail@tommartensen.de>
2017-05-09 10:17:56 +0200

    changed spaces into tabs

--- src/config/config-template.js
+++ src/config/config-template.js
@@ -18,7 +18,7 @@ const config = {
 	descriptions: {
 		textNumberOfEvents: "Number of events",
         exampleQuery: "INSERT INTO ExampleType SELECT * FROM PATTERN [[2] SimpleType]",
-        queryInputFieldHint: "Esper EPL Query"
+		queryInputFieldHint: "Esper EPL Query"
 	},
 	messages: {
 		notFound: "Error 404. The requested page does not exist. You found a glitch in the matrix.",
