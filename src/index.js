4f37029d3f29e266db45b6265e385799bfc14929
Frederike Ramin <frederike.ramin@student.hpi.de>
2017-05-09 12:25:42 +0200

    Fix rebase errors

--- src/index.js
+++ src/index.js
@@ -1,29 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
-import GridView from './GridView/GridView';
-import DetailView from './DetailView/DetailView';
-import SettingsView from './SettingsView/SettingsView';
-import NotFound from "./Utils/NotFound";
-import NewEventTypeView from "./CreateEventTypeView/CreateEventTypeView";
-import CreateEventQueryView from "./CreateEventQueryView/CreateEventQueryView";
-
-ReactDOM.render(
-	<Router>
-		<App>
-			<Switch>
-				<Route exact path="/" render={() => {
-					return (<Redirect to="/grid/-1"/>);
-				}} />
-				<Route path="/grid/:entityId" component={GridView}/>
-				<Route path="/details/:parentId/:entityId" component={DetailView}/>
-			    <Route exact path="/settings" component={SettingsView}/>
-				<Route path="/settings/eventType/:eventTypeId/eventQuery/create" component={CreateEventQueryView}/>
-				<Route path="/create/eventType" component={NewEventTypeView}/>
-				<Route path="*" component={NotFound}/>
-			</Switch>
-		</App>
-	</Router>,
-	document.getElementById('root')
-);
+import React from 'react';import ReactDOM from 'react-dom';import App from './App';import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';import GridView from './GridView/GridView';import DetailView from './DetailView/DetailView';import SettingsView from './SettingsView/SettingsView';import NotFound from "./Utils/NotFound";import NewEventTypeView from "./CreateEventTypeView/CreateEventTypeView";import CreateEventQueryView from "./CreateEventQueryView/CreateEventQueryView";ReactDOM.render(	<Router>		<App>			<Switch>				<Route exact path="/" render={() => {					return (<Redirect to="/grid/-1"/>);				}} />				<Route path="/grid/:entityId" component={GridView}/>				<Route path="/details/:parentId/:entityId" component={DetailView}/>			    <Route exact path="/settings" component={SettingsView}/>				<Route path="/settings/eventType/:eventTypeId/eventQuery/create" component={CreateEventQueryView}/>				<Route path="/settings/eventType/create" component={NewEventTypeView}/>				<Route path="*" component={NotFound}/>			</Switch>		</App>	</Router>,	document.getElementById('root'));
