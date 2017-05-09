5f528476d8fe9a3827943c1b9cd02265651170cc
Sven <sven.ladusch@web.de>
2017-05-09 13:11:29 +0200

    fixed indets

--- src/config/config-template.js
+++ src/config/config-template.js
@@ -26,10 +26,10 @@ const config = {
 		noEventTypes: "No event types",
 		actionRejected: "The requested action has been rejected."
 	},
-    status: {
-        "RUNNING": {color: "#4CAF50", colorLight: '#81C784'},
-        "FAILURE": {color: "#E53935", colorLight: "#E57373"},
-    },
+	status: {
+		"RUNNING": {color: "#4CAF50", colorLight: '#81C784'},
+		"FAILURE": {color: "#E53935", colorLight: "#E57373"},
+	},
 };
 
 export default config;
