0e65c5c6ad1fe6009a786aa1aea48940d2e05cb3
Tom Martensen <mail@tommartensen.de>
2017-05-09 08:39:46 +0200

    small refactoring in view

--- src/CreateEventQueryView/CreateEventQueryView.js
+++ src/CreateEventQueryView/CreateEventQueryView.js
@@ -2,17 +2,34 @@ import React from 'react';
 import {connect, PromiseState} from 'react-refetch';
 import ConnectionComponent from './../Utils/ConnectionComponent.js';
 import {Col, Container, Row} from "react-grid-system";
-import Header from './../Header';
 import {RaisedButton} from "material-ui";
 import IconSave from "material-ui/svg-icons/content/save";
 import IconCancel from "material-ui/svg-icons/navigation/cancel";
+import Header from './../Header';
+import { css } from 'aphrodite';
 import EventTypeInformation from "./EventTypeInformation";
 import EventQueryInputArea from "./EventQueryInputArea";
 import config from "../config/config";
-import { css } from 'aphrodite';
 import AppStyles from "../AppStyles";
 
 class CreateEventQueryView extends ConnectionComponent {
+
+    static getButtons() {
+        return (<div className={css(AppStyles.textAlignCenter)}>
+            <RaisedButton
+                label="Abort"
+                icon={<IconCancel/>}
+                className={css(AppStyles.marginAllSites)}
+                secondary={true}
+            />
+            <RaisedButton
+                label="Save"
+                icon={<IconSave/>}
+                className={css(AppStyles.marginAllSites)}
+                primary={true}
+            />
+        </div>);
+    }
 
     render() {
         const allFetches = PromiseState.all([this.props.eventType, this.props.attributes]);
@@ -34,20 +51,7 @@ class CreateEventQueryView extends ConnectionComponent {
                             <EventQueryInputArea/>
                         </Col>
                     </Row>
-                    <div className={css(AppStyles.textAlignCenter)}>
-                        <RaisedButton
-                            label="Abort"
-                            icon={<IconCancel/>}
-                            className={css(AppStyles.marginAllSites)}
-                            secondary={true}
-                        />
-                        <RaisedButton
-                            label="Save"
-                            icon={<IconSave/>}
-                            className={css(AppStyles.marginAllSites)}
-                            primary={true}
-                        />
-                    </div>
+                    {CreateEventQueryView.getButtons()}
                 </Container>
             </div>
         );
