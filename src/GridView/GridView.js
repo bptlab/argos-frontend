1e41ca787069b4305f8b914cc3c3ceceff9102d6
Julian <julian.weise@student.hpi.uni-potsdam.de>
2017-05-11 15:06:33 +0200

    Improved code formatting

--- src/GridView/GridView.js
+++ src/GridView/GridView.js
@@ -11,27 +11,27 @@ import config from './../config/config.js';
 import Header from "../Header";
 
 class GridView extends ConnectionComponent {
-    constructor() {
-        super();
-        this.getChildEntityTypes = this.getChildEntityTypes.bind(this);
-        this.getEntityType = this.getEntityType.bind(this);
-    }
+	constructor() {
+		super();
+		this.getChildEntityTypes = this.getChildEntityTypes.bind(this);
+		this.getEntityType = this.getEntityType.bind(this);
+	}
 
-    getEntityType(entity) {
-        const entityTypes = window.hierarchy.find(hierarchyArray => {
-            return hierarchyArray.find(entityType => {
-                return entityType.Id === entity.TypeId;
-            });
-        });
-        if (entityTypes) {
-            return entityTypes[0];
-        }
-    }
+	getEntityType(entity) {
+		const entityTypes = window.hierarchy.find(hierarchyArray => {
+			return hierarchyArray.find(entityType => {
+				return entityType.Id === entity.TypeId;
+			});
+		});
+		if (entityTypes) {
+			return entityTypes[0];
+		}
+	}
 
 	getChildEntityTypes(parentEntityTypeId) {
-		let childEntities  = [];
+		let childEntities = [];
 		window.hierarchy.forEach(hierarchyLayer => {
-			childEntities = childEntities.concat(hierarchyLayer.filter( (entityType) => {
+			childEntities = childEntities.concat(hierarchyLayer.filter((entityType) => {
 				return entityType.ParentId === parentEntityTypeId;
 			}));
 		});
@@ -39,44 +39,44 @@ class GridView extends ConnectionComponent {
 	}
 
 	getPageTitle(entity) {
-        const entityType = this.getEntityType(entity);
-        if (entityType) {
-            return entityType.Name + ": " + entity.Name;
-        }
-        return "Home";
+		const entityType = this.getEntityType(entity);
+		if (entityType) {
+			return entityType.Name + ": " + entity.Name;
+		}
+		return "Home";
 	}
 
 	render() {
 		const allFetches = PromiseState.all([this.props.entity]);
 		const entity = this.props.entity.value;
 		const connectionIncomplete = super.render(allFetches);
-		if(connectionIncomplete) {
+		if (connectionIncomplete) {
 			return connectionIncomplete;
 		}
 		const childEntityTypes = this.getChildEntityTypes(entity.TypeId);
 		return (
 			<div>
-				<Header title={this.getPageTitle(entity)} status={entity.Status} />
+				<Header title={this.getPageTitle(entity)} status={entity.Status}/>
 				<Container>
-				<HierarchyStepper
-					hierarchy={window.hierarchy}
-					currentEntity={entity}
-					getEntityType={this.getEntityType}
-					getChildEntityTypes={this.getChildEntityTypes}/>
-				<SearchBar/>
-				{childEntityTypes.map((childEntityType) => {
-					return(
-						<div key={`div-${childEntityType.Id}`}>
-							<h1>{childEntityType.Name}</h1>
-							<DonutChart styles={[AppStyles.elementMarginTop]} />
-							<CardGrid
-								styles={[AppStyles.elementMarginTop]}
-								key={childEntityType.Id}
-								currentEntity={entity}
-								entityType={childEntityType} />
-						</div>
-					);
-				})}
+					<HierarchyStepper
+						hierarchy={window.hierarchy}
+						currentEntity={entity}
+						getEntityType={this.getEntityType}
+						getChildEntityTypes={this.getChildEntityTypes}/>
+					<SearchBar/>
+					{childEntityTypes.map((childEntityType) => {
+						return (
+							<div key={`div-${childEntityType.Id}`}>
+								<h1>{childEntityType.Name}</h1>
+								<DonutChart styles={[AppStyles.elementMarginTop]}/>
+								<CardGrid
+									styles={[AppStyles.elementMarginTop]}
+									key={childEntityType.Id}
+									currentEntity={entity}
+									entityType={childEntityType}/>
+							</div>
+						);
+					})}
 				</Container>
 			</div>
 		);
