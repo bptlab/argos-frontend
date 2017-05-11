7c902fb758e5d257d669e92655909e0f4b9839c4
Frederike Ramin <frederike.ramin@student.hpi.de>
2017-05-11 11:26:51 +0200

    Fix rebase errors

--- src/GridView/GridView.js
+++ src/GridView/GridView.js
@@ -11,86 +11,23 @@ import config from './../config/config.js';
 import Header from "../Header";
 
 class GridView extends ConnectionComponent {
-	constructor() {
-		super();
-		this.getChildEntityTypes = this.getChildEntityTypes.bind(this);
-		this.getEntityType = this.getEntityType.bind(this);
-	}
+    constructor() {
+        super();
+        this.getChildEntityTypes = this.getChildEntityTypes.bind(this);
+        this.getEntityType = this.getEntityType.bind(this);
+    }
 
-	getEntityType(entity, hierarchy) {
-		const entityTypes = hierarchy.find(hierarchyArray => {
-			return hierarchyArray.find(entityType => {
-				return entityType.Id === entity.TypeId;
-			});
-		});
-		if (entityTypes) {
-			return entityTypes[0];
-		}
-	}
+    getEntityType(entity) {
+        const entityTypes = window.hierarchy.find(hierarchyArray => {
+            return hierarchyArray.find(entityType => {
+                return entityType.Id === entity.TypeId;
+            });
+        });
+        if (entityTypes) {
+            return entityTypes[0];
+        }
+    }
 
-	getChildEntityTypes(parentEntityTypeId, hierarchy) {
-		let childEntities  = [];
-		window.hierarchy.forEach(hierarchyLayer => {
-			childEntities = childEntities.concat(hierarchyLayer.filter( (entityType) => {
-				return entityType.ParentId === parentEntityTypeId;
-			}));
-		});
-		return childEntities;
-	}
-
-	getPageTitle(entity, hierarchy) {
-		const entityType = this.getEntityType(entity, hierarchy);
-		if (entityType) {
-			return entityType.Name + ": " + entity.Name;
-		}
-		return "Home";
-	}
-
-	render() {
-		const allFetches = PromiseState.all([this.props.entity]);
-		const entity = this.props.entity.value;
-		const connectionIncomplete = super.render(allFetches);
-		if(connectionIncomplete) {
-			return connectionIncomplete;
-		}
-		const childEntityTypes = this.getChildEntityTypes(entity.TypeId, window.hierarchy);
-		return (
-			<div>
-				<Header title={this.getPageTitle(entity, window.hierarchy)} status={entity.Status} />
-				<Container>
-					<HierarchyStepper
-						hierarchy={hierarchy}
-						currentEntity={entity}
-						getEntityType={this.getEntityType}
-						getChildEntityTypes={this.getChildEntityTypes}/>
-					<SearchBar/>
-					{childEntityTypes.map((childEntityType) => {
-						return(
-							<div key={`div-${childEntityType.Id}`}>
-								<h1>{childEntityType.Name}</h1>
-								<DonutChart styles={[AppStyles.elementMarginTop]} />
-								<CardGrid
-									styles={[AppStyles.elementMarginTop]}
-									key={childEntityType.Id}
-									currentEntity={entity}
-									entityType={childEntityType} />
-							</div>
-						);
-					})}
-				</Container>
-			</div>
-		);
-	}
-}
-
-export default connect.defaults({fetch: ConnectionComponent.switchFetch})(props => ({
-	entity: config.backendRESTRoute + `/entity/${props.match.params.entityId}`
-}))(GridView);
-
-class GridView extends ConnectionComponent {
-
-	var
-	
 	getChildEntityTypes(parentEntityTypeId) {
 		let childEntities  = [];
 		window.hierarchy.forEach(hierarchyLayer => {
@@ -102,15 +39,11 @@ class GridView extends ConnectionComponent {
 	}
 
 	getPageTitle(entity) {
-		const entityTypeInformation = window.hierarchy.find(hierarchyArray => {
-			return hierarchyArray.find(entityType => {
-				return entityType.Id === entity.TypeId;
-			});
-		});
-		if (entityTypeInformation) {
-			return entityTypeInformation[0].Name + ": " + entity.Name;
-		}
-		return "Home";
+        const entityType = this.getEntityType(entity);
+        if (entityType) {
+            return entityType.Name + ": " + entity.Name;
+        }
+        return "Home";
 	}
 
 	render() {
@@ -123,11 +56,13 @@ class GridView extends ConnectionComponent {
 		const childEntityTypes = this.getChildEntityTypes(entity.TypeId, window.hierarchy);
 		return (
 			<div>
-				<Header title={this.getPageTitle(entity, window.hierarchy)} status={entity.Status} />
+				<Header title={this.getPageTitle(entity)} status={entity.Status} />
 				<Container>
 				<HierarchyStepper
 					hierarchy={window.hierarchy}
-					currentEntityTypeId={entity.TypeId}/>
+					currentEntity={entity}
+					getEntityType={this.getEntityType}
+					getChildEntityTypes={this.getChildEntityTypes}/>
 				<SearchBar/>
 				{childEntityTypes.map((childEntityType) => {
 					return(
--- src/SettingsView/SettingsView.js
+++ src/SettingsView/SettingsView.js
@@ -13,7 +13,6 @@ import IconButton from 'material-ui/IconButton';
 import IconAdd from 'material-ui/svg-icons/content/add';
 import config from './../config/config';
 
-
 class SettingsView extends ConnectionComponent {
 
 	getCreateButton() {
@@ -36,68 +35,7 @@ class SettingsView extends ConnectionComponent {
 								showExpandableButton={true}/>
 							<CardText expandable={true}>
 								<Container>
-									<SearchBar fullWidth="true"/>
-									{this.props.eventTypes.value.map((eventType) => {
-										return(<EventType
-											eventType={eventType}
-											key={eventType.Id}
-											deleteEventType={this.props.deleteEventType}
-											fullWidth="true"/>);
-									})}
-								</Container>
-							</CardText>
-							<CardActions>
-								<IconButton href="settings/eventType/create"><IconAdd/></IconButton>
-							</CardActions>
-						</Card>
-					</Row>
-				</Container>
-			</div>
-		);
-	}
-}
-
-export default connect.defaults({fetch: ConnectionComponent.switchFetch})(() => ({
-	eventTypes: config.backendRESTRoute + `/eventtypes`,
-	deleteEventType: eventType => ({
-		postLikeResponse: {
-			url: config.backendRESTRoute + `/eventtype/${eventType.Id}/delete`,
-			method: 'DELETE',
-			body: "",
-			andThen: () => ({
-				eventTypes: {
-					url: config.backendRESTRoute + `/eventtypes`,
-					refreshing: true
-				}
-			})
-		}
-	})
-}))(SettingsView);
-
-
-class SettingsView extends ConnectionComponent {
-
-	getCreateButton() {
-		return <IconButton href="settings/eventType/create"><IconAdd/></IconButton>
-	}
-	render() {
-		const connectionIncomplete = super.render(this.props.eventTypes);
-		if(connectionIncomplete) {
-			return connectionIncomplete;
-		}
-		return (
-			<div>
-				<Header title="Settings"/>
-				<Container className={css(AppStyles.elementMarginTop)}>
-					<Row>
-						<Card initiallyExpanded={true}>
-							<CardHeader
-								title="Event Types"
-								actAsExpander={true}
-								showExpandableButton={true}/>
-							<CardText expandable={true}>
-								<Container>
-									<SearchBar fullWidth="true"/>
+									<SearchBar fullWidth={true}/>
 									{this.props.eventTypes.value.map((eventType) => {
 										return(<EventType
 											eventType={eventType}
