faeb9aaee7b303c20f49f3f7b9e287e11f7c70b1
Christian Friedow <christian@friedow.com>
2017-05-11 09:37:03 +0200

    Purged chart.js library

--- src/DetailView/EventDiagram.js
+++ src/DetailView/EventDiagram.js
@@ -1,50 +1,11 @@
 import React, { Component } from 'react';
-import config from './../config/config';
 import { css } from 'aphrodite';
-import Chart from 'chart.js';
 
 class EventDiagram extends Component {
-	componentDidMount() {
-		const chart = document.getElementById("event-diagram");
-		const data = {
-			labels: ["January", "February", "March", "April", "May", "June", "July"],
-			datasets: [
-				{
-					label: "Event Type 1",
-					lineTension: 0,
-					borderColor: config.colors.diagramLine,
-					borderCapStyle: 'butt',
-					borderDash: [],
-					borderDashOffset: 0.0,
-					borderJoinStyle: 'miter',
-					fill: true,
-					backgroundColor: config.colors.diagramBackground,
-					pointRadius: 1,
-					pointHitRadius: 10,
-					pointBackgroundColor: config.colors.diagramLine,
-					pointBorderWidth: 0,
-					pointHoverRadius: 3,
-					pointHoverBackgroundColor: config.colors.diagramLine,
-					pointHoverBorderWidth: 2,
-					pointHoverBorderColor: config.colors.diagramLine,
-					data: [65, 59, 80, 81, 56, 55, 40],
-					spanGaps: false,
-				}
-			]
-		};
-		const options = {
-
-		};
-		Chart.Line(chart, {
-			data: data,
-			options: options
-		});
-	}
-
 	render() {
 		return (
 			<div className={css(this.props.styles)}>
-				<canvas id="event-diagram" width="400" height="315"/>
+
 			</div>
 		);
 	}
