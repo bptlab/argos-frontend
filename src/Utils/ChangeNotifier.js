7a1459cc2bd136dafd268370133f5150dd0b3c9e
Tom Martensen <mail@tommartensen.de>
2017-05-18 11:11:33 +0200

    fixing bugs in changeNotifier

--- src/Utils/ChangeNotifier.js
+++ src/Utils/ChangeNotifier.js
@@ -8,38 +8,47 @@ class ChangeNotifier  {
 			this.webSocket.onerror = this.handleError;
 			this.webSocket.onopen = this.handleNewConnection;
 			this.webSocket.onclose = this.handleConnectionClose;
-			this.webSocket.onmessage = this.handleMessage;
+			this.webSocket.onmessage = this.handleMessage.bind(this);
 		}
 		this.subscribers = [];
 		this.handleMessage = this.handleMessage.bind(this);
 		this.register = this.register.bind(this);
 		this.unregisterAll = this.unregisterAll.bind(this);
 	}
-	
-	handleNewConnection(event) {
-		console.log("[WEBSOCKET] Established a new connection! Details: "+event.data);
+
+	static isRelevantNotification(subscription, notification) {
+		return ChangeNotifier.isRelevantNotificationForGridView(subscription, notification);
+        /*return (subscription.artifactType === notification.ArtifactType &&
+        ((notification.ArtifactType === "Event" && subscription.artifactId === notification.EntityId)
+        || subscription.artifactId === notification.ArtifactId));*/
+    }
+
+    static isRelevantNotificationForGridView(subscription, notification) {
+        return notification.ArtifactType === "Entity" && subscription.artifactId === notification.ArtifactId;
+    }
+
+	handleNewConnection() {
+		console.log("[WEBSOCKET] Established a new connection! Connection is open.");
 	}
 	
-	handleError(event) {
-		console.log("[WEBSOCKET] An error occured! Details: "+event.data);
+	handleError() {
+		console.log("[WEBSOCKET] An error occured! Connection could not be established.");
 	}
 	
 	handleMessage(event) {
 		const serverNotifications = JSON.parse(event.data);
-		serverNotifications.forEach((serverNotification) => {
-			this.subscribers.forEach((notificationNote) => {
-				if (notificationNote.artifactType === serverNotification.ArtifactType && 
-						((serverNotification.ArtifactType === "Event" &&
-						notificationNote.artifactId === serverNotification.EntityId) || 
-						notificationNote.artifactId === serverNotification.ArtifcatId)) {
-							notificationNote.notificationCallback();
+		serverNotifications.forEach((notification) => {
+			this.subscribers.forEach((subscription) => {
+				console.log(subscription);
+				if (ChangeNotifier.isRelevantNotification(subscription, notification)) {
+                    subscription.notificationCallback();
 				}
 			});
 		});
 	}
 
-	handleConnectionClose(event) {
-		console.log("[WEBSOCKET] Connection closed! Details: "+event.data);
+	handleConnectionClose() {
+		console.log("[WEBSOCKET] Connection closed!");
 	}
 	
 	register(artifactType, artifactId, callback) {
