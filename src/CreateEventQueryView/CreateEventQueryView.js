b65f677a93bb2b7e3bc6e1a257b522bacd399203
Tom Martensen <mail@tommartensen.de>
2017-05-24 11:13:07 +0200

    refactors render methods to reduce code duplication

--- src/CreateEventQueryView/CreateEventQueryView.js
+++ src/CreateEventQueryView/CreateEventQueryView.js
@@ -32,8 +32,10 @@ class CreateEventQueryView extends ConnectionComponent {
 		this.handleDescriptionInput = this.handleDescriptionInput.bind(this);
 		this.submitNewQuery = this.submitNewQuery.bind(this);
 		this.submitUpdatedQuery = this.submitUpdatedQuery.bind(this);
+		this.getComponentBody = this.getComponentBody.bind(this);
 	}
-	
+
+	// handles query changes in create view
 	handleCreateQueryInput(event) {
 		this.setState({
 			query: event.target.value,
@@ -41,19 +43,20 @@ class CreateEventQueryView extends ConnectionComponent {
 		});
 	}
 
+	// handles query changes in edit view, does a little string manipulation
 	handleEditQueryInput(event) {
 		const oldQuery = this.state.query;
 		if (oldQuery !== "") {
-            const uneditableQueryPart = Utils.splitStringBeforeSubString(oldQuery, "FROM ");
-            this.setState({
-                query: uneditableQueryPart + "FROM " + event.target.value,
-                queryErrorMessage: ''
-            });
-        } else {
-            this.setState({
-                query: this.props.eventQuery.value.Query
-            });
-        }
+			const uneditableQueryPart = Utils.splitStringBeforeSubString(oldQuery, "FROM ");
+			this.setState({
+				query: uneditableQueryPart + "FROM " + event.target.value,
+				queryErrorMessage: ''
+			});
+		} else {
+			this.setState({
+				query: this.props.eventQuery.value.Query
+			});
+		}
 	}
 
 	handleDescriptionInput(event) {
@@ -62,7 +65,7 @@ class CreateEventQueryView extends ConnectionComponent {
 			descriptionErrorMessage: ''
 		});
 	}
-	
+
 	isInvalidInput() {
 		let validationErrorOccured = false;
 		if(!this.state.queryDescription) {
@@ -80,16 +83,18 @@ class CreateEventQueryView extends ConnectionComponent {
 		return validationErrorOccured;
 	}
 
+	// submit handler for the create view
 	submitNewQuery() {
-        if (!this.isInvalidInput()) {
-            this.props.createEventQuery({
-                EventTypeId: this.props.match.params.eventTypeId,
-                Description: this.state.queryDescription,
-                Query: this.state.query
-            });
-        }
+		if (!this.isInvalidInput()) {
+			this.props.createEventQuery({
+				EventTypeId: this.props.match.params.eventTypeId,
+				Description: this.state.queryDescription,
+				Query: this.state.query
+			});
+		}
 	}
 
+	// submit handler for the edit view
 	submitUpdatedQuery() {
 		if (!this.isInvalidInput()) {
 			this.props.editEventQuery({
@@ -103,139 +108,128 @@ class CreateEventQueryView extends ConnectionComponent {
 		window.history.back();
 	}
 
-    componentWillMount() {
-        if (!this.isCreateView) {
-            this.props.lazyLoadEventQuery();
-        }
-    }
+	static handleOptionalActions(optionalActions) {
+		if(optionalActions && optionalActions.fulfilled) {
+			window.history.back();
+			return null;
+		}
+	}
 
-	renderCreateEventQuery() {
-        const allFetches = PromiseState.all([this.props.eventType, this.props.attributes]);
-        const optionalActions = this.props.createEventQueryResponse;
-        if(optionalActions && optionalActions.fulfilled) {
-            window.history.back();
-            return null;
-        }
-        const eventType = this.props.eventType.value;
-        const attributes = this.props.attributes.value;
-        const connectionIncomplete = super.render(allFetches);
-        if (connectionIncomplete) {
-            return connectionIncomplete;
-        }
+	static displayOptionalErrorMessage(optionalActions) {
+		return optionalActions && optionalActions.rejected && <ErrorMessage message={optionalActions.reason} />;
+	}
+
+	getAbortButton() {
+		return <RaisedButton
+			label="Abort"
+			icon={<IconCancel/>}
+			className={css(AppStyles.marginAllSites)}
+			secondary={true}
+			onClick={this.abort}
+		/>;
+	}
+
+	/**
+	 * Returns the body of the component to render. Fills some handlers and values.
+	 * @param attributes - attributes of the event type
+	 * @param queryInputChangeHandler - handler that is called when the query input field is changed
+	 * @param submitFormHandler - handler that is called on submit of the form
+	 * @param eventQuery (optional) - used to fill default values in edit view
+	 * @returns {XML}
+	 */
+	getComponentBody(attributes, queryInputChangeHandler, submitFormHandler, eventQuery) {
 		return (
-			<div>
-				<Header title={"Create Event Query for " + eventType.Name}/>
-				<div className={AppStyles.elementMarginTop}>
-					<Container>
-						<Row>
-							<Col md={4}>
-								<EventTypeInformation attributes={attributes}/>
-							</Col>
-							<Col md={8}>
-								{optionalActions && optionalActions.rejected &&
-									<ErrorMessage message={optionalActions.reason} />
-								}
-								<EventQueryInputArea
-									handleQueryInputChange={this.handleCreateQueryInput}
-									queryErrorMessage={this.state.queryErrorMessage}
-									handleDescriptionInputChange={this.handleDescriptionInput}
-									descriptionErrorMessage={this.state.descriptionErrorMessage}
-								/>
-							</Col>
-						</Row>
-						<div className={css(AppStyles.textAlignCenter)}>
-							<RaisedButton
-								label="Abort"
-								icon={<IconCancel/>}
-								className={css(AppStyles.marginAllSites)}
-								secondary={true}
-								onClick={this.abort}
+			<div className={AppStyles.elementMarginTop}>
+				<Container>
+					<Row>
+						<Col md={4}>
+							<EventTypeInformation attributes={attributes}/>
+						</Col>
+						<Col md={8}>
+							{CreateEventQueryView.displayOptionalErrorMessage()}
+							<EventQueryInputArea
+								handleQueryInputChange={queryInputChangeHandler}
+								queryErrorMessage={this.state.queryErrorMessage}
+								handleDescriptionInputChange={this.handleDescriptionInput}
+								descriptionErrorMessage={this.state.descriptionErrorMessage}
+								eventQuery={eventQuery}
 							/>
-							<RaisedButton
-								label="Save"
-								icon={<IconSave/>}
-								className={css(AppStyles.marginAllSites)}
-								primary={true}
-								onClick={this.submitNewQuery}
-							/>
-						</div>
-					</Container>
-				</div>
+						</Col>
+					</Row>
+					<div className={css(AppStyles.textAlignCenter)}>
+						{this.getAbortButton()}
+						<RaisedButton
+							label="Save"
+							icon={<IconSave/>}
+							className={css(AppStyles.marginAllSites)}
+							primary={true}
+							onClick={submitFormHandler}
+						/>
+					</div>
+				</Container>
 			</div>
 		);
 	}
 
-    eventQueryPending() {
-        if (!this.props.eventQuery) {
-            return <LoadingAnimation />;
-        }
-        const eventQueryConnection = super.render(this.props.eventQuery);
-        if (eventQueryConnection) {
-            return eventQueryConnection;
-        }
-        if (!this.props.eventQuery.value) {
-            return <LoadingAnimation />;
-        }
-        return null;
-    }
+	componentWillMount() {
+		if (!this.isCreateView) {
+			this.props.lazyLoadEventQuery();
+		}
+	}
 
+	// render method for the create event query view
+	renderCreateEventQuery() {
+		const optionalActions = this.props.createEventQueryResponse;
+		CreateEventQueryView.handleOptionalActions(optionalActions);
+		const allFetches = PromiseState.all([this.props.eventType, this.props.attributes]);
+		const connectionIncomplete = super.render(allFetches);
+		if (connectionIncomplete) {
+			return connectionIncomplete;
+		}
+		const eventType = this.props.eventType.value;
+		const attributes = this.props.attributes.value;
+		return (
+			<div>
+				<Header title={"Create Event Query for " + eventType.Name}/>
+				{this.getComponentBody(attributes, this.handleCreateQueryInput, this.submitNewQuery)}
+			</div>
+		);
+	}
+
+	// is called to load the existing query in the edit view
+	eventQueryPending() {
+		if (!this.props.eventQuery) {
+			return <LoadingAnimation />;
+		}
+		const eventQueryConnection = super.render(this.props.eventQuery);
+		if (eventQueryConnection) {
+			return eventQueryConnection;
+		}
+		if (!this.props.eventQuery.value) {
+			return <LoadingAnimation />;
+		}
+		return null;
+	}
+
+	// render method for the edit event query view
 	renderEditEventQuery() {
-        const allFetches = PromiseState.all([this.props.eventType, this.props.attributes]);
-        const optionalActions = this.props.editEventQueryResponse;
-        if(optionalActions && optionalActions.fulfilled) {
-            window.history.back();
-            return null;
-        }
-        const eventQueryPending = this.eventQueryPending();
-        if (eventQueryPending) {
-            return eventQueryPending;
-        }
-        const eventType = this.props.eventType.value;
-        const attributes = this.props.attributes.value;
-        const connectionIncomplete = super.render(allFetches);
-        if (connectionIncomplete) {
-            return connectionIncomplete;
-        }
+		const optionalActions = this.props.editEventQueryResponse;
+		CreateEventQueryView.handleOptionalActions(optionalActions);
+		const eventQueryPending = this.eventQueryPending();
+		if (eventQueryPending) {
+			return eventQueryPending;
+		}
+		const allFetches = PromiseState.all([this.props.eventType, this.props.attributes]);
+		const connectionIncomplete = super.render(allFetches);
+		if (connectionIncomplete) {
+			return connectionIncomplete;
+		}
+		const eventType = this.props.eventType.value;
+		const attributes = this.props.attributes.value;
 		return (
 			<div>
 				<Header title={"Edit Event Query for " + eventType.Name}/>
-				<div className={AppStyles.elementMarginTop}>
-					<Container>
-						<Row>
-							<Col md={4}>
-								<EventTypeInformation attributes={attributes}/>
-							</Col>
-							<Col md={8}>
-                                {optionalActions && optionalActions.rejected &&
-									<ErrorMessage message={optionalActions.reason} />
-                                }
-								<EventQueryInputArea
-									handleQueryInputChange={this.handleEditQueryInput}
-									queryErrorMessage={this.state.queryErrorMessage}
-									handleDescriptionInputChange={this.handleDescriptionInput}
-									descriptionErrorMessage={this.state.descriptionErrorMessage}
-									eventQuery={this.props.eventQuery.value}
-								/>
-							</Col>
-						</Row>
-						<div className={css(AppStyles.textAlignCenter)}>
-							<RaisedButton
-								label="Abort"
-								icon={<IconCancel/>}
-								className={css(AppStyles.marginAllSites)}
-								secondary={true}
-								onClick={this.abort}
-							/>
-							<RaisedButton
-								label="Save"
-								icon={<IconSave/>}
-								className={css(AppStyles.marginAllSites)}
-								primary={true}
-								onClick={this.submitUpdatedQuery}
-							/>
-						</div>
-					</Container>
-				</div>
+				{this.getComponentBody(attributes, this.handleEditQueryInput, this.submitUpdatedQuery, this.props.eventQuery.value)}
 			</div>
 		);
 	}
@@ -252,7 +246,7 @@ export default ConnectionComponent.argosConnector({fetch: ConnectionComponent.sw
 	eventType: config.backendRESTRoute + `/eventtype/${props.match.params.eventTypeId}`,
 	attributes: config.backendRESTRoute + `/eventtype/${props.match.params.eventTypeId}/attributes`,
 	lazyLoadEventQuery: () => ({
-        eventQuery: config.backendRESTRoute + `/eventquery/${props.match.params.eventQueryId}`,
+		eventQuery: config.backendRESTRoute + `/eventquery/${props.match.params.eventQueryId}`,
 	}),
 	createEventQuery: (body) => ({
 		createEventQueryResponse: {
@@ -261,11 +255,11 @@ export default ConnectionComponent.argosConnector({fetch: ConnectionComponent.sw
 			body: JSON.stringify(body)
 		}
 	}),
-    editEventQuery: (body) => ({
-        editEventQueryResponse: {
-            url: config.backendRESTRoute + `/eventquery/${props.match.params.eventQueryId}/edit`,
-            method: 'PUT',
-            body: JSON.stringify(body)
-        }
-    })
+	editEventQuery: (body) => ({
+		editEventQueryResponse: {
+			url: config.backendRESTRoute + `/eventquery/${props.match.params.eventQueryId}/edit`,
+			method: 'PUT',
+			body: JSON.stringify(body)
+		}
+	})
 }))(CreateEventQueryView);
