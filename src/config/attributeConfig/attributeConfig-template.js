7ede5a42076527a03c16a035402c1432794c62b0
Tom Martensen <mail@tommartensen.de>
2017-05-30 11:08:02 +0200

    explanation for attributeConfig

--- src/config/attributeConfig/attributeConfig-template.js
+++ src/config/attributeConfig/attributeConfig-template.js
@@ -1,5 +1,7 @@
 /*
-Comment 
+This is the config for displaying the attributes. It must have the same EntityTypes like the static data in the
+backend. Then, according to the requirements, the attributes, that should be displayed on the GridView are named and
+assigned the value 1 (integer).
  */
 const attributeConfig = {
 	"TransportationMode": {
