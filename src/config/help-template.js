8efbe677038009ee7ed317212cfb2e24fdf888b9
Maximilian <maximilian.voelker@student.hpi.de>
2017-05-30 09:49:39 +0200

    add hints to eventQueryView

--- src/config/help-template.js
+++ src/config/help-template.js
@@ -2,6 +2,13 @@ const help = {
 	display: {
 		hierarchyStepper: "This gives an overview of the hierarchy, showing the types of parent entities and possible types of child entities.<br><br>It can also be used to navigate back to parental types.",
 		entityAttributes: "Shows most important attributes and their values for the open entity.",
+		settingsView: {
+			eventTypes: "This is a list containing all defined event types. Here you can add, edit or delete their mappings and queries. If an event type is no longer needed, you may delete it here too",
+			eventTypeAttributes: "Shows a list containing all attributes defined on the selected event type.",
+			eventTypeQueries: "Shows a list of all queries defined on the selected event type. They can be edited and deleted here.<br><br>"
+			+ "Explanation: A query defines the rules when a new event is triggered and how the attributes are composed.",
+			entityMappings: "All defined entity mappings are displayed here. New mappings can be added and existing ones edited or deleted."
+		},
 	},
 	input: {
 		toggleChildrenEvents: "If turned on, all events from subsidiary entities are shown as well.",
@@ -10,6 +17,18 @@ const help = {
 		+ "To specify in which column(s) the search should be performed, separate the column name with \":\" from the search term.<br><br>"
 		+ "Example: \"ab\" will list all events having any attribute containing this string. \"Attr1:ab\" will show all events having \"ab\" in the value of \"Attr1\".",
 		gridViewFilterBar: "Filter all childentities by their name or attribute values.",
+		eventQueryView: {
+			description: "Provide a short description for this query.",
+			query: "Define the query when events for the current event type should be triggered. For the queries, only EPL is accepted."
+			+ " You may find an extensive EPL documentation <a href=\"http://www.espertech.com/esper/release-5.3.0/esper-reference/html/epl_clauses.html#epl-intro\">here.</a><br><br>"
+			+ "Example:<br>"
+			+ "INSERT INTO newEventType SELECT * FROM eventType1 WHERE attribute1=\"alert\"<br><br>"
+			+ "Explanation:<br>"
+			+ "'INSERT INTO newEventType' will create new events for newEventType each time the query matches.<br>"
+			+ "'SELECT *' selects all attributes from eventType1 to be included into events of the current event type.<br>"
+			+ "'FROM eventType1' defines that this query should be evaluated for each event occurring for eventType1.<br>"
+			+ "'WHERE attribute1=\"alert\"' filters all events so the query will only match if the event has \"alert\" in its value for attribute1."
+		}
 	},
 	button: {
 		inspectEntity: "Click to view details and events for this entity.",
