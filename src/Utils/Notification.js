6696759598ee1db7bbceacb3840c9e8e8e60ae07
Frederike Ramin <frederike.ramin@student.hpi.de>
2017-05-30 16:17:59 +0200

    Trigger notification event

--- src/Utils/Notification.js
+++ src/Utils/Notification.js
@@ -16,20 +16,39 @@ class Notification extends Component {
 		}
 	};
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			message: props.message,
+			mode: props.mode
+		};
+		this.handleTriggeredNotification = this.handleTriggeredNotification.bind(this);
+		window.addEventListener('notificationTriggered', this.handleTriggeredNotification, false);
+	}
+
+	handleTriggeredNotification() {
+		this.setState({
+			message: JSON.parse(window.sessionStorage.getItem('notificationMessage')).message,
+			mode: JSON.parse(window.sessionStorage.getItem('notificationMessage')).mode
+		});
+	}
+
 	static addSnackbarNotificationOnReferrer(message, mode) {
 		window.sessionStorage.setItem('notificationMessage',
 			JSON.stringify({message: message, targetPage: document.referrer, mode: mode, timestamp: new Date()}));
+		window.dispatchEvent(new Event('notificationTriggered'));
 	}
 
 	static addSnackbarNotificationOnSelf(message, mode) {
 		window.sessionStorage.setItem('notificationMessage',
 			JSON.stringify({message: message, targetPage: window.location.href, mode: mode, timestamp: new Date()}));
+		window.dispatchEvent(new Event('notificationTriggered'));
 	}
 
 	getIconForMode() {
-		if(this.props.mode.color === Notification.ModeEnum.SUCCESS.color) {
+		if(this.state.mode.color === Notification.ModeEnum.SUCCESS.color) {
 			return <IconSuccess className={css(AppStyles.verticalAlignMarginRight, AppStyles.colorWhite)}/>;
-		} else if (this.props.mode === Notification.ModeEnum.ERROR) {
+		} else if (this.state.mode === Notification.ModeEnum.ERROR) {
 			return <IconError className={css(AppStyles.verticalAlignMarginRight, AppStyles.colorWhite)}/>;
 		}
 	}
@@ -41,7 +60,7 @@ class Notification extends Component {
 		return (
 			<Snackbar
 				open={this.props.open}
-				message={<span className={css(AppStyles.dFlex, AppStyles.alignItemsCenter)}>{this.getIconForMode()}<p className={css(AppStyles.capitalizeFirstLetter, AppStyles.lineHeight25)}>{this.props.message}</p> </span>}
+				message={<span className={css(AppStyles.dFlex, AppStyles.alignItemsCenter)}>{this.getIconForMode()}<p className={css(AppStyles.capitalizeFirstLetter, AppStyles.lineHeight25)}>{this.state.message}</p> </span>}
 				autoHideDuration={config.notificationDisplayDuration}
 				bodyStyle={{backgroundColor: this.props.mode.color}}/>);
 	}
