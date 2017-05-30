4c006dbc737b785be9aa42576092f9a76a92d021
Tom Martensen <mail@tommartensen.de>
2017-05-30 11:03:50 +0200

    add new config

---
+++ src/config/attributeConfig/attributeConfig-template.js
@@ -0,0 +1,17 @@
+/*
+Comment 
+ */
+const attributeConfig = {
+	"TransportationMode": {
+		"isFarePaying": 1
+	},
+	"Line": {
+		"serviceTypes": 1
+	},
+	"StopPoint": {
+		"lat": 1,
+		"long": 1
+	}
+};
+
+export default attributeConfig;
